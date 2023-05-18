b2265aae7bd085ad5948bdf0c4f84a3cd1aab031
Eric Seidel <github@eseidel.com>
2023-05-17 21:00:38 -0700

    chore: refactor PriceData a little
    Moved the free functions to be static functions on PriceData.

--- packages/space_traders_cli/lib/prices.dart
+++ packages/space_traders_cli/lib/prices.dart
@@ -5,25 +5,6 @@ import 'package:http/http.dart' as http;
 import 'package:space_traders_api/api.dart';
 import 'package:space_traders_cli/logger.dart';
 
-List<Price> _parsePrices(String prices) {
-  final parsed = jsonDecode(prices) as List<dynamic>;
-  return parsed
-      .map<Price>((e) => Price.fromJson(e as Map<String, dynamic>))
-      .toList();
-}
-
-List<Price>? _loadPricesCache() {
-  final pricesFile = File('prices.json');
-  if (pricesFile.existsSync()) {
-    return _parsePrices(pricesFile.readAsStringSync());
-  }
-  return null;
-}
-
-void _savePricesCache(String prices) {
-  File('prices.json').writeAsStringSync(prices);
-}
-
 // {"waypointSymbol": "X1-ZS60-53675E", "symbol": "IRON_ORE", "supply":
 // "ABUNDANT", "purchasePrice": 6, "sellPrice": 4, "tradeVolume": 1000,
 // "timestamp": "2023-05-14T21:52:56.530126100+00:00"}
@@ -78,26 +59,60 @@ class Price {
 /// A collection of price records.
 class PriceData {
   /// Create a new price data collection.
-  PriceData(this.prices);
+  PriceData(this.prices, this.cacheFilePath);
 
   // Eventually we should keep our own data and not use the global data.
   /// Url from which to fetch the global price data.
-  static Uri uri = Uri.parse('https://st.feba66.de/prices');
+  static const String defaultUrl = 'https://st.feba66.de/prices';
+
+  /// The default path to the cache file.
+  static const String defaultCacheFilePath = 'prices.json';
+
+  /// The path to the cache file.
+  final String cacheFilePath;
 
   /// The list of price records.
   final List<Price> prices;
 
+  static List<Price> _parsePrices(String prices) {
+    final parsed = jsonDecode(prices) as List<dynamic>;
+    return parsed
+        .map<Price>((e) => Price.fromJson(e as Map<String, dynamic>))
+        .toList();
+  }
+
+  static PriceData? _loadPricesCache(String cacheFilePath) {
+    final pricesFile = File(cacheFilePath);
+    if (pricesFile.existsSync()) {
+      return PriceData(
+        _parsePrices(pricesFile.readAsStringSync()),
+        cacheFilePath,
+      );
+    }
+    return null;
+  }
+
+  static void _savePricesCache({
+    required String jsonString,
+    required String cacheFilePath,
+  }) {
+    File(cacheFilePath).writeAsStringSync(jsonString);
+  }
+
   /// Load the price data from the cache or from the url.
-  static Future<PriceData> load() async {
+  static Future<PriceData> load({String? cacheFilePath, String? url}) async {
+    final uri = Uri.parse(url ?? defaultUrl);
+    final filePath = cacheFilePath ?? defaultCacheFilePath;
     // Try to load prices.json.  If it does not exist, pull down and cache
     // from the url.
-    var prices = _loadPricesCache();
-    if (prices == null) {
-      logger.info("Couldn't load prices from cache, fetching from $uri");
-      final response = await http.get(uri);
-      prices = _parsePrices(response.body);
-      _savePricesCache(response.body);
+    final fromCache = _loadPricesCache(filePath);
+    if (fromCache != null) {
+      return fromCache;
     }
-    return PriceData(prices);
+    logger.info("Couldn't load prices from cache, fetching from $uri");
+    final response = await http.get(uri);
+    final data = PriceData(_parsePrices(response.body), filePath);
+    _savePricesCache(jsonString: response.body, cacheFilePath: filePath);
+    return data;
   }
 }
