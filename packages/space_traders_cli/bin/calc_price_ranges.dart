3c1d56960cacafcbcb06e7c3e9ff0f2caafda328
Eric Seidel <github@eseidel.com>
2023-05-17 20:18:36 -0700

    feat: add super-basic stats printing to calc_price_ranges.dart

--- packages/space_traders_cli/bin/calc_price_ranges.dart
+++ packages/space_traders_cli/bin/calc_price_ranges.dart
@@ -2,7 +2,9 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:http/http.dart' as http;
+import 'package:space_traders_api/api.dart';
 import 'package:space_traders_cli/logger.dart';
+import 'package:stats/stats.dart';
 
 List<Price> _parsePrices(String prices) {
   final parsed = jsonDecode(prices) as List<dynamic>;
@@ -57,6 +59,23 @@ class Price {
   final DateTime timestamp;
 }
 
+void printPriceRanges(List<Price> gameStats) {
+  void printStats(String label, Iterable<int> values) {
+    final stats = Stats.fromData(values);
+    print('$label: ${stats.withPrecision(3)}');
+  }
+
+  for (final tradeSymbol in TradeSymbol.values) {
+    final trades =
+        gameStats.where((p) => p.symbol == tradeSymbol.value).toList();
+    if (trades.isEmpty) {
+      continue;
+    }
+    printStats('$tradeSymbol purchase', trades.map((p) => p.purchasePrice));
+    printStats('$tradeSymbol sell', trades.map((p) => p.sellPrice));
+  }
+}
+
 void main(List<String> args) async {
   final uri = Uri.parse('https://st.feba66.de/prices');
 
@@ -71,4 +90,5 @@ void main(List<String> args) async {
   }
 
   logger.info('${prices.length} prices loaded.');
+  printPriceRanges(prices);
 }
