2f1b880830fac4fcb71ceea6d1139b5a417c89a5
Eric Seidel <github@eseidel.com>
2023-05-20 16:33:05 -0700

    feat: Add median price information to buy_ship
    Removed list_available_ships since buy_ship was strictly better.

--- packages/space_traders_cli/bin/buy_ship.dart
+++ packages/space_traders_cli/bin/buy_ship.dart
@@ -5,21 +5,28 @@ import 'package:space_traders_cli/actions.dart';
 import 'package:space_traders_cli/auth.dart';
 import 'package:space_traders_cli/extensions.dart';
 import 'package:space_traders_cli/logger.dart';
+import 'package:space_traders_cli/prices.dart';
 import 'package:space_traders_cli/printing.dart';
 import 'package:space_traders_cli/queries.dart';
 
-String describeShipType(ShipType type, Shipyard shipyard) {
+String describeShipType(ShipType type, Shipyard shipyard, PriceData priceData) {
 // This assumes there is only one ship available of each type in the yard.
   final ship = shipyard.ships.firstWhereOrNull(
     (s) => s.type == type,
   );
-  final priceString = ship == null ? '' : ' - ${ship.purchasePrice}';
-  return '$type$priceString';
+  final actualPriceString = ship == null ? '' : ' - ${ship.purchasePrice}';
+  final medianPrice = priceData.medianPurchasePrice(type.value);
+  final medianPriceString = medianPrice == null
+      ? ''
+      : ' - median price: ${creditsString(medianPrice)}';
+  return '$type$actualPriceString$medianPriceString';
 }
 
 void main(List<String> args) async {
   const fs = LocalFileSystem();
   final api = defaultApi(fs);
+
+  final priceData = await PriceData.load(fs);
 
   final agentResult = await api.agents.getMyAgent();
 
@@ -49,7 +56,7 @@ void main(List<String> args) async {
   final shipType = logger.chooseOne(
     'Which type?',
     choices: shipyard.shipTypes.map((t) => t.type!).toList(),
-    display: (s) => describeShipType(s, shipyard),
+    display: (s) => describeShipType(s, shipyard, priceData),
   );
 
   logger.info('Purchasing $shipType.');
