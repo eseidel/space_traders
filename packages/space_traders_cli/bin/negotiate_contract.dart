f8035b85d313f2b6ecd94520d7f96fa420936890
Eric Seidel <github@eseidel.com>
2023-05-22 06:44:15 -0700

    feat: add bin/negotiate_contract.dart
    This can get new contracts.  Not wired into the main loop yet.

---
+++ packages/space_traders_cli/bin/negotiate_contract.dart
@@ -0,0 +1,27 @@
+import 'package:file/local.dart';
+import 'package:space_traders_cli/auth.dart';
+import 'package:space_traders_cli/extensions.dart';
+import 'package:space_traders_cli/logger.dart';
+import 'package:space_traders_cli/printing.dart';
+import 'package:space_traders_cli/queries.dart';
+
+void main(List<String> args) async {
+  const fs = LocalFileSystem();
+  final api = defaultApi(fs);
+
+  final agentResult = await api.agents.getMyAgent();
+  final agent = agentResult!.data;
+  final hq = parseWaypointString(agent.headquarters);
+  final systemWaypoints = await waypointsInSystem(api, hq.system).toList();
+
+  final myShips = await allMyShips(api).toList();
+  final ship = logger.chooseOne(
+    'Which ship?',
+    choices: myShips,
+    display: (ship) => shipDescription(ship, systemWaypoints),
+  );
+
+  final response = await api.fleet.negotiateContract(ship.symbol);
+  final contract = response!.data;
+  logger.info(contract.toString());
+}
