e78047206d984c50826d498557a36665d9d11bab
Eric Seidel <github@eseidel.com>
2023-05-27 10:41:40 -0700

    Commit my unfinished scripts
    Neither of these are tested (one is fully commented out), but committing to move between machines.

---
+++ packages/space_traders_cli/bin/nearest_deal_for.dart
@@ -0,0 +1,124 @@
+import 'package:file/local.dart';
+import 'package:space_traders_api/api.dart';
+import 'package:space_traders_cli/auth.dart';
+import 'package:space_traders_cli/extensions.dart';
+import 'package:space_traders_cli/logger.dart';
+import 'package:space_traders_cli/prices.dart';
+import 'package:space_traders_cli/queries.dart';
+
+// This should end up sharing code with Deal, Route, etc.
+class _Availability {
+  const _Availability({
+    required this.tradeSymbol,
+    required this.marketSymbol,
+    required this.sellPrice,
+    required this.jumps,
+  });
+  final String tradeSymbol;
+  final String marketSymbol;
+  final int sellPrice;
+  final int jumps;
+}
+
+Stream<_Availability> _availabilityInSystem(
+  Api api,
+  PriceData priceData,
+  Agent agent,
+  List<Waypoint> systemWaypoints,
+  TradeSymbol tradeSymbol,
+  int jumps,
+) async* {
+  final marketplaceWaypoints =
+      systemWaypoints.where((w) => w.hasMarketplace).toList();
+
+  for (final marketplaceWaypoint in marketplaceWaypoints) {
+    final prices = priceData
+        .sellPricesFor(
+          tradeSymbol: tradeSymbol.value,
+          marketSymbol: marketplaceWaypoint.symbol,
+        )
+        .toList();
+    if (prices.isEmpty) {
+      if (priceData.hasRecentMarketData(marketplaceWaypoint.symbol)) {
+        logger.info(
+          '${marketplaceWaypoint.symbol} does not have $tradeSymbol.',
+        );
+      } else {
+        logger.info(
+          'No recent price data for ${marketplaceWaypoint.symbol}',
+        );
+      }
+      continue;
+    }
+    yield _Availability(
+      tradeSymbol: tradeSymbol.value,
+      marketSymbol: marketplaceWaypoint.symbol,
+      sellPrice: prices.first.sellPrice,
+      jumps: jumps,
+    );
+  }
+}
+
+/// Look through nearby marketplaces (including ones a jump away)
+/// looking for the best deal for a given symbol.
+void main(List<String> args) async {
+  const fs = LocalFileSystem();
+  final api = defaultApi(fs);
+  final waypointCache = WaypointCache(api);
+
+  final priceData = await PriceData.load(fs);
+
+  final promptResponse = logger.prompt(
+    'Which trade symbol? (Options: ${TradeSymbol.values.join(', ')}))',
+  );
+
+  final tradeSymbol = TradeSymbol.fromJson(promptResponse.toUpperCase().trim());
+  if (tradeSymbol == null) {
+    logger.err('Invalid trade symbol');
+    return;
+  }
+  final agentResult = await api.agents.getMyAgent();
+  final agent = agentResult!.data;
+  final hq = parseWaypointString(agent.headquarters);
+  final systemWaypoints = await waypointCache.waypointsInSystem(hq.system);
+
+  final availabilityList = <_Availability>[
+    ...await _availabilityInSystem(
+      api,
+      priceData,
+      agent,
+      systemWaypoints,
+      tradeSymbol,
+      0,
+    ).toList(),
+  ];
+
+  final jumpGateWaypoint =
+      systemWaypoints.firstWhere((w) => w.type == WaypointType.JUMP_GATE);
+
+  final jumpGateResponse =
+      await api.systems.getJumpGate(hq.system, jumpGateWaypoint.symbol);
+  final jumpGate = jumpGateResponse!.data;
+  for (final system in jumpGate.connectedSystems) {
+    logger.info('${system.symbol} - ${system.distance}');
+    final waypoints = await waypointCache.waypointsInSystem(system.symbol);
+    availabilityList.addAll(
+      await _availabilityInSystem(
+        api,
+        priceData,
+        agent,
+        waypoints,
+        tradeSymbol,
+        1,
+      ).toList(),
+    );
+  }
+
+  availabilityList.sort((a, b) => a.sellPrice.compareTo(b.sellPrice));
+  for (final availability in availabilityList) {
+    logger.info(
+      '${availability.marketSymbol} - ${availability.sellPrice}'
+      ' - ${availability.jumps}',
+    );
+  }
+}
---
+++ packages/space_traders_cli/bin/nearest_uncharted.dart
@@ -0,0 +1,126 @@
+import 'dart:core';
+
+
+// Future<List<String>?> _innerRouteToNearestUnchartedWaypoint(
+//   Api api,
+//   Set<String> consideredSystems,
+//   String waypointSymbol,
+// ) async {
+//   final systemWaypoints = 
+//  await waypointsInSystem(api, currentSystem).toList();
+//   final currentWaypoint = lookupWaypoint(waypointSymbol, systemWaypoints);
+
+//   final potentialRoutes = <(Waypoint, List<String>)>[
+//     (currentWaypoint, [waypointSymbol])
+//   ];
+
+//   while (potentialRoutes.isNotEmpty) {
+//     final (waypoint, route) = potentialRoutes.removeAt(0);
+//     final currentSystem = parseWaypointString(waypointSymbol).system;
+//     consideredSystems.add(waypoint.system);
+//     for node in
+//   }
+
+//  queue = [(start,[start])]
+// visited = set()
+
+// while queue:
+//     vertex, path = queue.pop(0)
+//     visited.add(vertex)
+//     for node in graph[vertex]:
+//         if node == end:
+//             return path + [end]
+//         else:
+//             if node not in visited:
+//                 visited.add(node)
+//                 queue.append((node, path + [node]))
+
+// Check our current system first.
+// If it has an uncharted waypoint, return closest?
+// final unchartedWaypoints = systemWaypoints
+//     .where((w) => w.type != WaypointType.JUMP_GATE && w.chart == null)
+//     .toList();
+// if (unchartedWaypoints.isNotEmpty) {
+//   final sorted =
+//       unchartedWaypoints.sortedBy<num>((w) => w.distanceTo(currentWaypoint));
+//   return [sorted.first.symbol];
+// }
+// If it does not, we check what systems are connected via jumpgate
+// Starting at the closest one, we check if it has an unexplored waypoint.
+// If none of those do, we look at second jump potentials from the original
+// list and repeat the process.
+
+// See if any of these are unexplored.
+
+//  1  procedure BFS(G, root) is
+//  2      let Q be a queue
+//  3      label root as explored
+//  4      Q.enqueue(root)
+//  5      while Q is not empty do
+//  6          v := Q.dequeue()
+//  7          if v is the goal then
+//  8              return v
+//  9          for all edges from v to w in G.adjacentEdges(v) do
+// 10              if w is not labeled as explored then
+// 11                  label w as explored
+// 12                  w.parent := v
+// 13                  Q.enqueue(w)
+
+//   final jumpGateWaypoint =
+//       systemWaypoints
+//  .firstWhereOrNull((w) => w.type == WaypointType.JUMP_GATE);
+//   final jumpGateResponse =
+//       await api.systems.getJumpGate(currentSystem, jumpGateWaypoint!.symbol);
+//   final jumpGate = jumpGateResponse!.data;
+//   final connectedSystems = jumpGate.connectedSystems
+//       .where((s) => !consideredSystems.contains(s.symbol))
+//       .sortedBy<num>((s) => s.distance)
+//       .toList();
+//   for (final system in connectedSystems) {
+//     logger.info('${system.symbol} - ${system.distance}');
+//     final waypoints = await waypointsInSystem(api, system.symbol).toList();
+//     printWaypoints(waypoints, indent: '  ');
+//   }
+//   return null;
+// }
+
+// Future<Route?> _routeToNearestUnchartedWaypoint(
+//   Api api,
+//   String waypointSymbol,
+// ) async {
+//   final consideredSystems = <String>{};
+//   final routeSymbols = await _innerRouteToNearestUnchartedWaypoint(
+//     api,
+//     consideredSystems,
+//     waypointSymbol,
+//   );
+//   if (routeSymbols == null) {
+//     return null;
+//   }
+//   return Route(routeSymbols);
+// }
+
+// void main(List<String> args) async {
+//   const fs = LocalFileSystem();
+//   final api = defaultApi(fs);
+
+//   final agentResult = await api.agents.getMyAgent();
+
+//   final agent = agentResult!.data;
+//   final hq = parseWaypointString(agent.headquarters);
+//   final systemWaypoints = await waypointsInSystem(api, hq.system).toList();
+
+//   final myShips = await allMyShips(api).toList();
+//   final ship = logger.chooseOne(
+//     'Which ship?',
+//     choices: myShips,
+//     display: (ship) => shipDescription(ship, systemWaypoints),
+//   );
+//   final maybeRoute =
+//       await _routeToNearestUnchartedWaypoint(api, ship.nav.waypointSymbol);
+//   if (maybeRoute == null) {
+//     logger.info('No route found');
+//     return;
+//   }
+//   logger.info(maybeRoute.toString());
+// }
