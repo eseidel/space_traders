4943a97d182e61de06bcaddecd141e9a8f1d3e81
Eric Seidel <github@eseidel.com>
2023-06-10 14:43:42 -0700

    Create show_shipyard.dart

---
+++ packages/space_traders_cli/bin/show_shipyard.dart
@@ -0,0 +1,32 @@
+import 'package:file/local.dart';
+import 'package:space_traders_cli/auth.dart';
+import 'package:space_traders_cli/logger.dart';
+import 'package:space_traders_cli/printing.dart';
+import 'package:space_traders_cli/queries.dart';
+import 'package:space_traders_cli/systems_cache.dart';
+import 'package:space_traders_cli/waypoint_cache.dart';
+
+void main(List<String> args) async {
+  const fs = LocalFileSystem();
+  final api = defaultApi(fs);
+  final systemsCache = await SystemsCache.load(fs);
+  final waypointCache = WaypointCache(api, systemsCache);
+
+  final myShips = await allMyShips(api).toList();
+  final ship = await chooseShip(api, waypointCache, myShips);
+
+  final shipyardWaypoints =
+      await waypointCache.shipyardWaypointsForSystem(ship.nav.systemSymbol);
+
+  final waypoint = logger.chooseOne(
+    'Which shipyard?',
+    choices: shipyardWaypoints,
+    display: waypointDescription,
+  );
+
+  final shipyardResponse =
+      await api.systems.getShipyard(waypoint.systemSymbol, waypoint.symbol);
+  final shipyard = shipyardResponse!.data;
+
+  prettyPrintJson(shipyard.toJson());
+}
