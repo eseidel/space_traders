9a972e2acf3db94a9634c297a398694e2cc50b9b
Eric Seidel <github@eseidel.com>
2023-06-11 08:40:31 -0700

    Support 2 digit ship numbers!

--- packages/space_traders_cli/lib/logger.dart
+++ packages/space_traders_cli/lib/logger.dart
@@ -11,22 +11,24 @@ void setVerboseLogging() {
   logger.level = Level.verbose;
 }
 
+String _shipLabel(Ship ship) => ship.emojiName.padRight(5);
+
 /// Log [message] for [ship] to the global logger at the detail level.
 void shipDetail(Ship ship, String message) {
-  logger.detail('${ship.emojiName} $message');
+  logger.detail('${_shipLabel(ship)} $message');
 }
 
 /// Log [message] for [ship] to the global logger at the info level.
 void shipInfo(Ship ship, String message) {
-  logger.info('${ship.emojiName} $message');
+  logger.info('${_shipLabel(ship)} $message');
 }
 
 /// Log [message] for [ship] to the global logger at the warning level.
 void shipWarn(Ship ship, String message) {
-  logger.warn('${ship.emojiName} $message');
+  logger.warn('${_shipLabel(ship)} $message');
 }
 
 /// Log [message] for [ship] to the global logger at the error level.
 void shipErr(Ship ship, String message) {
-  logger.err('${ship.emojiName} $message');
+  logger.err('${_shipLabel(ship)} $message');
 }
