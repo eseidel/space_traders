dcbd6ff4c8746fad7b64d65a6565c201e7b1303f
Eric Seidel <github@eseidel.com>
2023-06-11 14:16:04 -0700

    Create list_fleet.dart
    Now that we have a fleet which needs listing. :)

---
+++ packages/space_traders_cli/bin/list_fleet.dart
@@ -0,0 +1,35 @@
+import 'package:file/local.dart';
+import 'package:space_traders_api/api.dart';
+import 'package:space_traders_cli/auth.dart';
+import 'package:space_traders_cli/logger.dart';
+import 'package:space_traders_cli/printing.dart';
+import 'package:space_traders_cli/queries.dart';
+import 'package:space_traders_cli/systems_cache.dart';
+import 'package:space_traders_cli/waypoint_cache.dart';
+
+void main(List<String> args) async {
+  const fs = LocalFileSystem();
+  final api = defaultApi(fs);
+  final systemsCache = await SystemsCache.load(fs);
+  final waypointCache = WaypointCache(api, systemsCache);
+
+  final ships = await allMyShips(api).toList();
+
+  // get all the ships
+  // count them by type.
+  final typeCounts = <ShipFrameSymbolEnum, int>{};
+  for (final ship in ships) {
+    final type = ship.frame.symbol;
+    typeCounts[type] = (typeCounts[type] ?? 0) + 1;
+  }
+
+  for (final type in typeCounts.keys) {
+    logger.info('$type: ${typeCounts[type]}');
+  }
+
+  // list them all.
+  final shipWaypoints = await waypointsForShips(waypointCache, ships);
+  for (final ship in ships) {
+    logger.info(shipDescription(ship, shipWaypoints));
+  }
+}
