cb7f479dc7e073c42c652c2c2742cb968a78710f
Eric Seidel <github@eseidel.com>
2023-06-10 20:47:48 -0700

    Create list_ship_prices.dart
    Lists the median prices of ships based on our shipyard_prices data.

---
+++ packages/space_traders_cli/bin/list_ship_prices.dart
@@ -0,0 +1,26 @@
+import 'dart:math';
+
+import 'package:file/local.dart';
+import 'package:space_traders_api/api.dart';
+import 'package:space_traders_cli/logger.dart';
+import 'package:space_traders_cli/printing.dart';
+import 'package:space_traders_cli/shipyard_prices.dart';
+
+void main(List<String> args) async {
+  const fs = LocalFileSystem();
+  final shipyardPrices = await ShipyardPrices.load(fs);
+
+  final maxNameLength =
+      ShipType.values.fold(0, (m, t) => max(m, t.value.length));
+
+  // Load up the ShipyardPrices.
+  // Walk through all ship types.
+  // Print median price for each.
+  for (final shipType in ShipType.values) {
+    final medianPrice = shipyardPrices.medianPurchasePrice(shipType);
+    final priceString =
+        (medianPrice == null ? '' : creditsString(medianPrice)).padLeft(13);
+    final name = shipType.value.substring('SHIP_'.length);
+    logger.info('${name.padRight(maxNameLength)} $priceString');
+  }
+}
