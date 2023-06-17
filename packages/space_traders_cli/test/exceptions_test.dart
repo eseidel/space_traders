b2718eb767f7920afb254e83e867552042937958
Eric Seidel <github@eseidel.com>
2023-06-17 16:44:18 -0700

    Add a few exception tests

--- packages/space_traders_cli/test/exceptions_test.dart
+++ packages/space_traders_cli/test/exceptions_test.dart
@@ -56,4 +56,97 @@ void main() {
     );
     expect(expiration3, isNull);
   });
+
+  test('isSurveyExhaustedException', () {
+    final exception = ApiException(
+      409,
+      jsonEncode({
+        'error': {
+          'message':
+              'Ship extract failed. Survey X1-VS75-67965Z-D0F7C6 has been exhausted.',
+          'code': 4224
+        }
+      }),
+    );
+    expect(isSurveyExhaustedException(exception), isTrue);
+  });
+
+  test('isSurveyExpiredException', () {
+    final exception = ApiException(
+      400,
+      jsonEncode({
+        'error': {
+          'message':
+              'Ship survey failed. Target signature is no longer in range or valid.',
+          'code': 4221
+        }
+      }),
+    );
+    expect(isSurveyExpiredException(exception), isTrue);
+  });
+
+  test('isAPIExceptionWithCode', () {
+    final exception = ApiException(
+      400,
+      jsonEncode({
+        'error': {
+          'message': 'Ship action is still on cooldown for 6 second(s).',
+          'code': 4000,
+          'data': {
+            'cooldown': {
+              'shipSymbol': 'ESEIDEL2-1',
+              'totalSeconds': 70,
+              'remainingSeconds': 6,
+              'expiration': '2023-05-20T22:53:30.735Z'
+            }
+          }
+        }
+      }),
+    );
+    expect(isAPIExceptionWithCode(exception, 4000), isTrue);
+  });
+
+  test('isMarketDoesNotSellFuelException', () {
+    final exception = ApiException(
+        400,
+        '{"error":{"message":"Market purchase failed. Trade good FUEL is '
+        'not available at '
+        'X1-BZ43-47417A.","code":4601,"data":{"waypointSymbol": '
+        '"X1-BZ43-47417A","tradeSymbol":"FUEL"}}}');
+    expect(isMarketDoesNotSellFuelException(exception), isTrue);
+  });
+
+  test('isInfuficientFuelException', () {
+    final exception = ApiException(
+        400,
+        '{"error":{"message":"Navigate request failed. Ship ESEIDEL-2 '
+        'requires 14 more fuel for '
+        'navigation.","code":4203,"data":{"shipSymbol":"ESEIDEL-2", '
+        '"fuelRequired":39,"fuelAvailable":25}}}');
+    expect(isInfuficientFuelException(exception), isTrue);
+  });
+
+  test('isMaintenanceWindowException', () {
+    final exception = ApiException(
+        503,
+        '{"error":{"message":"SpaceTraders is currently in maintenance mode '
+        'and unavailable. This will hopefully only last a few minutes while we '
+        'update or upgrade our servers. Check discord for updates '
+        'https://discord.com/invite/jh6zurdWk5 and consider donating to keep '
+        'the servers running '
+        'https://donate.stripe.com/28o29m5vxcri6OccMM","code":503}}');
+    expect(isMaintenanceWindowException(exception), isTrue);
+  });
+
+  test('isReservedHandleException', () {
+    final exception = ApiException(
+      400,
+      '{"error":{"message":'
+      '"Cannot register agent. Call sign has been reserved between resets: '
+      'ESEIDEL. If you reserved your call sign and cannot register, please '
+      'reach out on Discord, Twitter, or email us at '
+      'contact@spacetraders.io","code":4110}}',
+    );
+    expect(isReservedHandleException(exception), isTrue);
+  });
 }
