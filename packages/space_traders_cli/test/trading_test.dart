5db42d30feb4ce0d0a3d9929e8d1c93840ae6f19
Eric Seidel <github@eseidel.com>
2023-06-17 14:48:03 -0700

    Add null tests for estimateSellPrice and estimatePurchasePrice

--- packages/space_traders_cli/test/trading_test.dart
+++ packages/space_traders_cli/test/trading_test.dart
@@ -1,4 +1,5 @@
 import 'package:mocktail/mocktail.dart';
+import 'package:space_traders_cli/api.dart';
 import 'package:space_traders_cli/cache/prices.dart';
 import 'package:space_traders_cli/cache/waypoint_cache.dart';
 import 'package:space_traders_cli/trading.dart';
@@ -14,5 +15,18 @@ void main() {
     final finder = DealFinder(priceData);
     final deals = finder.findDeals();
     expect(deals, isEmpty);
+  });
+
+  test('estimateSellPrice', () {
+    final priceData = MockPriceData();
+    final estimate = estimateSellPrice(priceData, Market(symbol: 'A'), 'FUEL');
+    expect(estimate, null);
+  });
+
+  test('estimatePurchasePrice', () {
+    final priceData = MockPriceData();
+    final estimate =
+        estimatePurchasePrice(priceData, Market(symbol: 'A'), 'FUEL');
+    expect(estimate, null);
   });
 }
