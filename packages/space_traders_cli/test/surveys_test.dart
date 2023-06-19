9b5039df99a59585e5bf9f6638b5dff49bb4278c
Eric Seidel <github@eseidel.com>
2023-06-18 18:43:05 -0700

    test: add roundtrip tests for SurveyData
    This also found and worked around a bug in JSON serialization for Survey.  It would ahve round-tripped correctly through jsonEncode to a string, but fails to correctly produce the expected maps due to not recursively calling toJson.

---
+++ packages/space_traders_cli/test/surveys_test.dart
@@ -0,0 +1,62 @@
+import 'package:file/memory.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:space_traders_cli/api.dart';
+import 'package:space_traders_cli/cache/surveys.dart';
+import 'package:test/test.dart';
+
+class MockShip extends Mock implements Ship {}
+
+void main() {
+  test('HistoricalSurvey roundtrip', () {
+    final moonLanding = DateTime.utc(1969, 7, 20, 20, 18, 04);
+    final survey = HistoricalSurvey(
+      exhausted: true,
+      survey: Survey(
+        symbol: 'MOON',
+        signature: 'sig',
+        expiration: moonLanding,
+        size: SurveySizeEnum.SMALL,
+        deposits: [
+          SurveyDeposit(
+            symbol: 'DIAMONDS',
+          ),
+        ],
+      ),
+      timestamp: moonLanding,
+    );
+    final json = survey.toJson();
+    final survey2 = HistoricalSurvey.fromJson(json);
+    final json2 = survey2.toJson();
+    // Despite HistoricalSurvey being immutable, Survey isn't so we
+    // can't write a working equals for HistoricalSurvey.
+    expect(survey2 != survey, true);
+    // But the json generated should be identical.
+    expect(json2, json);
+  });
+
+  test('SurveyData load/save', () async {
+    final moonLanding = DateTime.utc(1969, 7, 20, 20, 18, 04);
+    final fs = MemoryFileSystem.test();
+    final survey = HistoricalSurvey(
+      exhausted: true,
+      survey: Survey(
+        symbol: 'MOON',
+        signature: 'sig',
+        expiration: moonLanding,
+        size: SurveySizeEnum.SMALL,
+        deposits: [
+          SurveyDeposit(
+            symbol: 'DIAMONDS',
+          ),
+        ],
+      ),
+      timestamp: moonLanding,
+    );
+    final surveys = [survey];
+    final surveyData = SurveyData(surveys: surveys, fs: fs);
+    await surveyData.save();
+    final surveyData2 = await SurveyData.load(fs);
+    expect(surveyData2.surveys.length, surveys.length);
+    // Could also json compare.
+  });
+}
