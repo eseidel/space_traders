7bb83f1dce4c7173f2003bd1dfb9467304158953
Eric Seidel <github@eseidel.com>
2023-06-18 17:08:46 -0700

    test: Test some of the miner code
    There are two small changes in behavior here:
    1. Previously it would throw an exception if it can't find a nearby market.  Now it will re-route to a nearby system (untested).
    2. Previously it would happily mine with cargo it couldn't sell.  Now it will go to a second market if necessary to sell it (untested).
    
    Added some very basic tests of miner code and did a little bit of refactoring as part of the above.

---
+++ packages/space_traders_cli/test/miner_test.dart
@@ -0,0 +1,214 @@
+import 'package:mason_logger/mason_logger.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:space_traders_cli/api.dart';
+import 'package:space_traders_cli/behavior/behavior.dart';
+import 'package:space_traders_cli/behavior/miner.dart';
+import 'package:space_traders_cli/cache/agent_cache.dart';
+import 'package:space_traders_cli/cache/data_store.dart';
+import 'package:space_traders_cli/cache/prices.dart';
+import 'package:space_traders_cli/cache/surveys.dart';
+import 'package:space_traders_cli/cache/systems_cache.dart';
+import 'package:space_traders_cli/cache/transactions.dart';
+import 'package:space_traders_cli/cache/waypoint_cache.dart';
+import 'package:space_traders_cli/logger.dart';
+import 'package:test/test.dart';
+
+class MockShipNav extends Mock implements ShipNav {}
+
+class MockShipNavRoute extends Mock implements ShipNavRoute {}
+
+class MockApi extends Mock implements Api {}
+
+class MockDataStore extends Mock implements DataStore {}
+
+class MockAgentCache extends Mock implements AgentCache {}
+
+class MockShip extends Mock implements Ship {}
+
+class MockSystemsCache extends Mock implements SystemsCache {}
+
+class MockMarketCache extends Mock implements MarketCache {}
+
+class MockTransactionLog extends Mock implements TransactionLog {}
+
+class MockBehaviorManager extends Mock implements BehaviorManager {}
+
+class MockPriceData extends Mock implements PriceData {}
+
+class MockSurveyData extends Mock implements SurveyData {}
+
+class MockWaypointCache extends Mock implements WaypointCache {}
+
+class MockWaypoint extends Mock implements Waypoint {}
+
+class MockLogger extends Mock implements Logger {}
+
+void main() {
+  test('surveyWorthMining with no surveys', () async {
+    final priceData = MockPriceData();
+    final surveyData = MockSurveyData();
+    when(
+      () => surveyData.recentSurveysAtWaypoint(
+        count: any(named: 'count'),
+        waypointSymbol: any(named: 'waypointSymbol'),
+      ),
+    ).thenReturn([]);
+    final maybeSurvey = await surveyWorthMining(
+      priceData,
+      surveyData,
+      surveyWaypointSymbol: 'S-E-A',
+      nearbyMarketSymbol: 'S-E-A',
+    );
+    expect(maybeSurvey, isNull);
+  });
+
+  test('surveyWorthMining', () async {
+    final priceData = MockPriceData();
+    final surveyData = MockSurveyData();
+    final now = DateTime.timestamp();
+    final oneHourFromNow = now.add(const Duration(hours: 1));
+    final surveys = [
+      for (int i = 0; i < 10; i++)
+        HistoricalSurvey(
+          timestamp: now.subtract(Duration(seconds: i)),
+          exhausted: false,
+          survey: Survey(
+            expiration: oneHourFromNow,
+            signature: 'sig$i',
+            symbol: 'sym',
+            deposits: [
+              SurveyDeposit(
+                symbol: (i == 0) ? 'DIAMONDS' : 'ALUMINUM',
+              ),
+            ],
+            size: SurveySizeEnum.SMALL,
+          ),
+        )
+    ];
+    when(
+      () => surveyData.recentSurveysAtWaypoint(
+        count: any(named: 'count'),
+        waypointSymbol: any(named: 'waypointSymbol'),
+      ),
+    ).thenReturn(surveys);
+    when(
+      () => priceData.recentSellPrice(
+        marketSymbol: any(named: 'marketSymbol'),
+        tradeSymbol: 'DIAMONDS',
+      ),
+    ).thenReturn(100);
+    when(
+      () => priceData.recentSellPrice(
+        marketSymbol: any(named: 'marketSymbol'),
+        tradeSymbol: 'ALUMINUM',
+      ),
+    ).thenReturn(10);
+    final maybeSurvey = await surveyWorthMining(
+      priceData,
+      surveyData,
+      surveyWaypointSymbol: 'S-E-A',
+      nearbyMarketSymbol: 'S-E-A',
+    );
+    expect(maybeSurvey!.deposits.first.symbol, 'DIAMONDS');
+  });
+  test('nearestWaypointWithMarket returns start', () async {
+    final waypointCache = MockWaypointCache();
+    final start = MockWaypoint();
+    when(() => start.traits).thenReturn(
+      [
+        WaypointTrait(
+          symbol: WaypointTraitSymbolEnum.MARKETPLACE,
+          name: '',
+          description: '',
+        )
+      ],
+    );
+    final nearest = await nearestWaypointWithMarket(waypointCache, start);
+    expect(nearest, start);
+  });
+
+  test('nearestWaypointWithMarket null', () async {
+    final waypointCache = MockWaypointCache();
+    final start = MockWaypoint();
+    final market = MockWaypoint();
+    when(() => start.traits).thenReturn([]);
+    when(() => start.systemSymbol).thenReturn('S-E');
+    when(
+      () => waypointCache.waypointsInJumpRadius(
+        startSystem: any(named: 'startSystem'),
+        maxJumps: any(named: 'maxJumps'),
+      ),
+    ).thenAnswer((_) => Stream.fromIterable([market]));
+    when(() => market.traits).thenReturn([]);
+    final nearest = await nearestWaypointWithMarket(waypointCache, start);
+    expect(nearest, isNull);
+  });
+
+  test('nearestWaypointWithMarket', () async {
+    final waypointCache = MockWaypointCache();
+    final start = MockWaypoint();
+    final market = MockWaypoint();
+    when(() => start.traits).thenReturn([]);
+    when(() => start.systemSymbol).thenReturn('S-E');
+    when(
+      () => waypointCache.waypointsInJumpRadius(
+        startSystem: any(named: 'startSystem'),
+        maxJumps: any(named: 'maxJumps'),
+      ),
+    ).thenAnswer((_) => Stream.fromIterable([market]));
+    when(() => market.traits).thenReturn(
+      [
+        WaypointTrait(
+          symbol: WaypointTraitSymbolEnum.MARKETPLACE,
+          name: '',
+          description: '',
+        )
+      ],
+    );
+    final nearest = await nearestWaypointWithMarket(waypointCache, start);
+    expect(nearest, market);
+  });
+
+  // This is mostly a proof of concept that we can test advance* functions.
+  test('advanceMiner in transit', () async {
+    final api = MockApi();
+    final db = MockDataStore();
+    final priceData = MockPriceData();
+    final agentCache = MockAgentCache();
+    final ship = MockShip();
+    final systemsCache = MockSystemsCache();
+    final waypointCache = MockWaypointCache();
+    final marketCache = MockMarketCache();
+    final transactionLog = MockTransactionLog();
+    final behaviorManager = MockBehaviorManager();
+    final surveyData = MockSurveyData();
+    final shipNav = MockShipNav();
+    final shipNavRoute = MockShipNavRoute();
+
+    final arrivalTime = DateTime.timestamp();
+    when(() => ship.symbol).thenReturn('S');
+    when(() => ship.nav).thenReturn(shipNav);
+    when(() => shipNav.status).thenReturn(ShipNavStatus.IN_TRANSIT);
+    when(() => shipNav.waypointSymbol).thenReturn('W');
+    when(() => shipNav.route).thenReturn(shipNavRoute);
+    when(() => shipNavRoute.arrival).thenReturn(arrivalTime);
+
+    // TODO(eseidel): How do we mock globals during a test?
+    logger = MockLogger();
+    final waitUntil = await advanceMiner(
+      api,
+      db,
+      priceData,
+      agentCache,
+      ship,
+      systemsCache,
+      waypointCache,
+      marketCache,
+      transactionLog,
+      behaviorManager,
+      surveyData,
+    );
+    expect(waitUntil, arrivalTime);
+    verify(() => logger.info('🛸#S  ✈️  to W, 00:00:00 left')).called(1);
+  });
+}
