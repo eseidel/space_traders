baeb5f5c20ce531a43be061ef5088010fa77a97d
Eric Seidel <github@eseidel.com>
2023-07-25 12:12:19 +0000

    chore: add coverage for BehaviorState

--- packages/cli/test/behavior/behavior_test.dart
+++ packages/cli/test/behavior/behavior_test.dart
@@ -1,11 +1,41 @@
 import 'package:cli/api.dart';
 import 'package:cli/behavior/behavior.dart';
+import 'package:cli/behavior/deliver.dart';
+import 'package:cli/nav/route.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('BehaviorState JSON roundtrip', () async {
     const shipSymbol = ShipSymbol('S', 1);
-    final state = BehaviorState(shipSymbol, Behavior.buyShip);
+    // A BehaviorState would never have all these values at once, just testing
+    // that they all roundtrip.
+    final state = BehaviorState(
+      shipSymbol,
+      Behavior.buyShip,
+      buyJob: BuyJob(
+        tradeSymbol: TradeSymbol.ADVANCED_CIRCUITRY,
+        units: 1,
+        buyLocation: WaypointSymbol.fromString('S-A-W'),
+      ),
+      deliverJob: DeliverJob(
+        tradeSymbol: TradeSymbol.ADVANCED_CIRCUITRY,
+        waypointSymbol: WaypointSymbol.fromString('S-A-W'),
+      ),
+      routePlan: RoutePlan(
+        actions: [
+          RouteAction(
+            startSymbol: WaypointSymbol.fromString('S-A-W'),
+            endSymbol: WaypointSymbol.fromString('S-A-C'),
+            type: RouteActionType.navCruise,
+            duration: 10,
+          )
+        ],
+        fuelCapacity: 10,
+        shipSpeed: 10,
+        fuelUsed: 10,
+      ),
+      mountToAdd: ShipMountSymbolEnum.GAS_SIPHON_I,
+    );
     final json = state.toJson();
     final newState = BehaviorState.fromJson(json);
     expect(newState.shipSymbol, shipSymbol);
