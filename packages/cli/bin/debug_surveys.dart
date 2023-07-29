c965492fd97106be73278f9d281f5351446aab41
Eric Seidel <github@eseidel.com>
2023-07-29 21:34:04 +0000

    Add a script for debugging survey logic

---
+++ packages/cli/bin/debug_surveys.dart
@@ -0,0 +1,31 @@
+import 'package:cli/behavior/miner.dart';
+import 'package:cli/cache/caches.dart';
+import 'package:cli/cli.dart';
+import 'package:cli/logger.dart';
+
+Future<void> command(FileSystem fs, List<String> args) async {
+  final surveyData = await SurveyData.load(fs);
+  final marketPrices = MarketPrices.load(fs);
+  final agentCache = AgentCache.loadCached(fs)!;
+  final systemsCache = await SystemsCache.load(fs);
+
+  final hq = agentCache.agent.headquartersSymbol;
+  final hqSystemSymbol = hq.systemSymbol;
+  final systemSymbol = hqSystemSymbol;
+
+  final systemWaypoints = systemsCache.waypointsInSystem(systemSymbol);
+  final mineSymbol =
+      systemWaypoints.firstWhere((w) => w.canBeMined).waypointSymbol;
+
+  final survey = await surveyWorthMining(
+    marketPrices,
+    surveyData,
+    surveyWaypointSymbol: mineSymbol,
+    nearbyMarketSymbol: mineSymbol,
+  );
+  logger.info('$survey');
+}
+
+void main(List<String> args) async {
+  await runOffline(args, command);
+}
