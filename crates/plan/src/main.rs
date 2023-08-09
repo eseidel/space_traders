57769cab830b67857f565d435ffe772b5ec021e2
Eric Seidel <github@eseidel.com>
2023-08-08 17:09:43 -0700

    Use spacedust.rs

--- crates/plan/src/main.rs
+++ crates/plan/src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+// Get the list of ships.
+// Get the agent (for credits).
+
+use spacedust::apis::agents_api::get_my_agent;
+use spacedust::apis::configuration::Configuration;
+use spacedust::apis::default_api::register;
+use spacedust::models::register_request::RegisterRequest;
+use spacedust::models::FactionSymbols;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Create Configuration
+    let mut conf = Configuration::new();
+
+    // Create Register Request
+    let reg_req = RegisterRequest::new(FactionSymbols::Cosmic, "ESEIDEL-RUST".to_string());
+
+    // Register Agent
+    let register_response = register(&conf, Some(reg_req)).await;
+
+    match register_response {
+        Ok(res) => {
+            println!("{:#?}", res);
+            // Update Config with Agent Token
+            conf.bearer_access_token = Some(res.data.token);
+        }
+        Err(err_res) => {
+            panic!("{:#?}", err_res);
+        }
+    }
+
+    // Get Agent Details to Confirm Working
+    match get_my_agent(&conf).await {
+        Ok(res) => {
+            println!("{:#?}", res);
+            // Print Symbol
+            println!("My Symbol: {:#?}", res.data.symbol);
+        }
+        Err(err_res) => {
+            panic!("{:#?}", err_res);
+        }
+    }
+
+    Ok(())
 }
