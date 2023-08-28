1f9c9af9d6f0bf47acb37f589a12c239feecdea8
Eric Seidel <github@eseidel.com>
2023-08-28 03:42:19 +0000

    Use global coverage script

---
+++ coverage.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -e
+
+# A script for computing combined coverage for all packages in the repo.
+# This can be used for viewing coverage locally in your editor.
+
+PACKAGES='cli db server types ui'
+
+for PACKAGE_DIR in $PACKAGES
+do
+    echo $PACKAGE_DIR
+    cd packages/$PACKAGE_DIR
+    dart pub get
+    dart test --coverage=coverage
+    dart pub global run coverage:format_coverage --lcov --in=coverage --out=coverage/lcov.info --packages=.dart_tool/package_config.json --report-on=lib --check-ignore
+    cd ../..
+done
+
+dart pub global run combine_coverage --repo-path .
