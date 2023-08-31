bde0a3f276fb8cafc64463d34a01c02298ddff82
Eric Seidel <github@eseidel.com>
2023-08-31 10:25:27 +0000

    refactor: move byAddingTransactions test down to types

--- packages/cli/test/trading_test.dart
+++ packages/cli/test/trading_test.dart
@@ -392,69 +392,6 @@ void main() {
     expect(deals2.any((d) => d.contractId == 'foo'), isTrue);
   });
 
-  test('byAddingTransactions', () {
-    final start = WaypointSymbol.fromString('S-A-B');
-    final end = WaypointSymbol.fromString('S-A-C');
-    final deal = Deal.test(
-      sourceSymbol: start,
-      destinationSymbol: end,
-      tradeSymbol: TradeSymbol.FUEL,
-      purchasePrice: 1,
-      sellPrice: 2,
-    );
-    final costed = CostedDeal(
-      deal: deal,
-      cargoSize: 1,
-      transactions: [],
-      startTime: DateTime(2021),
-      route: RoutePlan(
-        actions: [
-          RouteAction(
-            startSymbol: start,
-            endSymbol: end,
-            type: RouteActionType.navCruise,
-            duration: 10,
-          ),
-        ],
-        fuelCapacity: 10,
-        fuelUsed: 10,
-        shipSpeed: 10,
-      ),
-      costPerFuelUnit: 100,
-    );
-    const shipSymbol = ShipSymbol('S', 1);
-    final transaction1 = Transaction(
-      transactionType: TransactionType.market,
-      shipSymbol: shipSymbol,
-      waypointSymbol: start,
-      tradeSymbol: TradeSymbol.FUEL,
-      shipType: null,
-      perUnitPrice: 10,
-      tradeType: MarketTransactionTypeEnum.PURCHASE,
-      quantity: 1,
-      timestamp: DateTime(2021),
-      agentCredits: 10,
-      accounting: AccountingType.fuel,
-    );
-    final transaction2 = Transaction(
-      transactionType: TransactionType.market,
-      shipSymbol: shipSymbol,
-      waypointSymbol: end,
-      tradeSymbol: TradeSymbol.FUEL,
-      shipType: null,
-      perUnitPrice: 10,
-      tradeType: MarketTransactionTypeEnum.SELL,
-      quantity: 1,
-      timestamp: DateTime(2021),
-      agentCredits: 10,
-      accounting: AccountingType.fuel,
-    );
-    final costed2 = costed.byAddingTransactions([transaction1]);
-    expect(costed2.transactions, [transaction1]);
-    final costed3 = costed2.byAddingTransactions([transaction2]);
-    expect(costed3.transactions, [transaction1, transaction2]);
-  });
-
   test('Deal.maxUnits', () {
     final start = WaypointSymbol.fromString('S-A-B');
     final end = WaypointSymbol.fromString('S-A-C');
--- packages/types/test/deal_test.dart
+++ packages/types/test/deal_test.dart
@@ -54,4 +54,67 @@ void main() {
     // Can't compare objects via equals because CostedDeal is not immutable.
     expect(json, json2);
   });
+
+  test('byAddingTransactions', () {
+    final start = WaypointSymbol.fromString('S-A-B');
+    final end = WaypointSymbol.fromString('S-A-C');
+    final deal = Deal.test(
+      sourceSymbol: start,
+      destinationSymbol: end,
+      tradeSymbol: TradeSymbol.FUEL,
+      purchasePrice: 1,
+      sellPrice: 2,
+    );
+    final costed = CostedDeal(
+      deal: deal,
+      cargoSize: 1,
+      transactions: [],
+      startTime: DateTime(2021),
+      route: RoutePlan(
+        actions: [
+          RouteAction(
+            startSymbol: start,
+            endSymbol: end,
+            type: RouteActionType.navCruise,
+            duration: 10,
+          ),
+        ],
+        fuelCapacity: 10,
+        fuelUsed: 10,
+        shipSpeed: 10,
+      ),
+      costPerFuelUnit: 100,
+    );
+    const shipSymbol = ShipSymbol('S', 1);
+    final transaction1 = Transaction(
+      transactionType: TransactionType.market,
+      shipSymbol: shipSymbol,
+      waypointSymbol: start,
+      tradeSymbol: TradeSymbol.FUEL,
+      shipType: null,
+      perUnitPrice: 10,
+      tradeType: MarketTransactionTypeEnum.PURCHASE,
+      quantity: 1,
+      timestamp: DateTime(2021),
+      agentCredits: 10,
+      accounting: AccountingType.fuel,
+    );
+    final transaction2 = Transaction(
+      transactionType: TransactionType.market,
+      shipSymbol: shipSymbol,
+      waypointSymbol: end,
+      tradeSymbol: TradeSymbol.FUEL,
+      shipType: null,
+      perUnitPrice: 10,
+      tradeType: MarketTransactionTypeEnum.SELL,
+      quantity: 1,
+      timestamp: DateTime(2021),
+      agentCredits: 10,
+      accounting: AccountingType.fuel,
+    );
+    final costed2 = costed.byAddingTransactions([transaction1]);
+    expect(costed2.transactions, [transaction1]);
+    final costed3 = costed2.byAddingTransactions([transaction2]);
+    expect(costed3.transactions, [transaction1, transaction2]);
+  });
 }
