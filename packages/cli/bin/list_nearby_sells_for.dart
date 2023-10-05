e64232b6237e3f26e8bb4931382d9d7e0fb7210a
Eric Seidel <github@eseidel.com>
2023-10-05 04:31:28 +0000

    feat: improve list_nearby_sells_for

--- packages/cli/bin/list_nearby_sells_for.dart
+++ packages/cli/bin/list_nearby_sells_for.dart
@@ -1,16 +1,31 @@
 import 'package:cli/cache/caches.dart';
 import 'package:cli/cli.dart';
 import 'package:cli/nav/route.dart';
+import 'package:cli/printing.dart';
 import 'package:cli/trading.dart';
+import 'package:types/types.dart';
 
 Future<void> command(FileSystem fs, ArgResults argResults) async {
   final marketPrices = MarketPrices.load(fs);
   final systemsCache = SystemsCache.loadCached(fs)!;
   final routePlanner = RoutePlanner.fromSystemsCache(systemsCache);
+  final agentCache = AgentCache.loadCached(fs)!;
   final shipCache = ShipCache.loadCached(fs)!;
 
-  final ship = shipCache.ships.first;
   const tradeSymbol = TradeSymbol.DIAMONDS;
+
+  final hq = agentCache.headquarters(systemsCache);
+  final hqMine = systemsCache
+      .waypointsInSystem(hq.systemSymbol)
+      .firstWhere((w) => w.isAsteroidField)
+      .waypointSymbol;
+
+  final ship = shipCache.ships.firstWhere((s) => s.isMiner);
+
+  // This hack is OK so long as we don't write the ShipCache to disk.
+  ship.nav.waypointSymbol = hqMine.waypoint;
+  ship.nav.systemSymbol = hqMine.system;
+  logger.info('Finding markets which buy $tradeSymbol near $hqMine.');
 
   // List all markets nearby which buy diamonds.
   final trips = marketsTradingSortedByDistance(
@@ -19,8 +34,19 @@ Future<void> command(FileSystem fs, ArgResults argResults) async {
     ship,
     tradeSymbol,
   );
+  final supplyWidth = MarketTradeGoodSupplyEnum.values.fold(0, (max, e) {
+    final width = e.toString().length;
+    return width > max ? width : max;
+  });
+  logger.info('Waypoint       Sell Supply    Volume   Round trip');
   for (final trip in trips) {
-    logger.info('Market: ${trip.route.endSymbol}, price: ${trip.price}');
+    final price = trip.price;
+    logger.info('${price.waypointSymbol.waypoint.padRight(14)} '
+        // sellPrice is the price we sell *to* the market.
+        '${creditsString(price.sellPrice).padLeft(4)} '
+        '${price.supply.toString().padRight(supplyWidth)} '
+        '${price.tradeVolume.toString().padLeft(6)} '
+        '${approximateDuration(trip.route.duration * 2).padLeft(4)}');
   }
 }
 
