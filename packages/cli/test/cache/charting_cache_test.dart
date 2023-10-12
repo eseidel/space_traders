79f4a3024bdb69d0a2f76f81d06bacd0d589f361
Eric Seidel <github@eseidel.com>
2023-10-11 20:53:40 -0700

    test: add test for ChartingCache

--- packages/cli/test/cache/charting_cache_test.dart
+++ packages/cli/test/cache/charting_cache_test.dart
@@ -1,9 +1,12 @@
 import 'package:cli/cache/charting_cache.dart';
+import 'package:cli/cache/systems_cache.dart';
 import 'package:cli/cache/waypoint_traits.dart';
 import 'package:file/memory.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 import 'package:types/types.dart';
+
+class _MockSystemsCache extends Mock implements SystemsCache {}
 
 class _MockWaypointTraitCache extends Mock implements WaypointTraitCache {}
 
@@ -38,5 +41,52 @@ void main() {
       loaded.values.first.traitSymbols.first,
       WaypointTraitSymbolEnum.ASH_CLOUDS,
     );
+  });
+
+  test('ChartingCache', () async {
+    final fs = MemoryFileSystem.test();
+    final waypointTraits = _MockWaypointTraitCache();
+    final waypointSymbol = WaypointSymbol.fromString('A-A-A');
+    final unchartedSymbol = WaypointSymbol.fromString('B-B-B');
+    final chartedWaypoint = Waypoint(
+      symbol: waypointSymbol.waypoint,
+      type: WaypointType.ASTEROID_FIELD,
+      systemSymbol: waypointSymbol.system,
+      x: 0,
+      y: 0,
+      // Chart is required for the cache to work.
+      chart: Chart(
+        waypointSymbol: waypointSymbol.waypoint,
+        submittedBy: 'ESEIDEL',
+        submittedOn: DateTime(2021),
+      ),
+    );
+    final unchartedWaypoint = Waypoint(
+      symbol: waypointSymbol.waypoint,
+      type: WaypointType.ASTEROID_FIELD,
+      systemSymbol: unchartedSymbol.system,
+      x: 0,
+      y: 0,
+      // No chart, so this won't be cached.
+    );
+    final chartingCache = ChartingCache({}, waypointTraits, fs: fs)
+      ..addWaypoints([
+        chartedWaypoint,
+        unchartedWaypoint,
+      ]);
+    expect(chartingCache.waypointCount, 1);
+    expect(chartingCache.valuesForSymbol(waypointSymbol), isNotNull);
+    expect(chartingCache.valuesForSymbol(unchartedSymbol), isNull);
+
+    final systemsCache = _MockSystemsCache();
+    when(() => systemsCache.waypointFromSymbol(waypointSymbol))
+        .thenReturn(chartedWaypoint.toSystemWaypoint());
+    final waypoint2 =
+        chartingCache.waypointFromSymbol(systemsCache, waypointSymbol);
+    expect(waypoint2?.chart, isNotNull);
+
+    final uncharted2 =
+        chartingCache.waypointFromSymbol(systemsCache, unchartedSymbol);
+    expect(uncharted2, isNull);
   });
 }
