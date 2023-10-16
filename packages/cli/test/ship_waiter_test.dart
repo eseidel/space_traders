4e698d841a2c4a94ef0be1bd085a2213a988e3ab
Eric Seidel <github@eseidel.com>
2023-10-16 08:14:22 -0700

    Revert "test: ShipWaiter priority"
    This reverts commit e35ff08551edb41e2af7dee85bbba954ce0f6728.

--- packages/cli/test/ship_waiter_test.dart
+++ packages/cli/test/ship_waiter_test.dart
@@ -28,39 +28,4 @@ void main() {
     expect(waiter.nextShip().shipSymbol, shortSymbol);
     expect(waiter.nextShip().shipSymbol, longSymbol);
   });
-
-  test('ShipWaiter priority', () {
-    final waiter = ShipWaiter();
-    const probeSymbol = ShipSymbol('a', 1);
-    const otherSymbol = ShipSymbol('a', 2);
-    final probe = _MockShip();
-    final probeFrame = _MockShipFrame();
-    when(() => probeFrame.symbol).thenReturn(ShipFrameSymbolEnum.PROBE);
-    when(() => probe.symbol).thenReturn(probeSymbol.symbol);
-    when(() => probe.frame).thenReturn(probeFrame);
-
-    final other = _MockShip();
-    final otherFrame = _MockShipFrame();
-    when(() => otherFrame.symbol).thenReturn(ShipFrameSymbolEnum.FIGHTER);
-    when(() => other.symbol).thenReturn(otherSymbol.symbol);
-    when(() => other.frame).thenReturn(otherFrame);
-
-    final now = DateTime.timestamp();
-    final longAgo = now.subtract(const Duration(days: 1));
-    final recent = now.subtract(const Duration(hours: 1));
-
-    waiter
-      ..scheduleShip(probe, longAgo)
-      ..scheduleShip(other, recent);
-    expect(waiter.nextShip().shipSymbol, otherSymbol);
-    expect(waiter.nextShip().shipSymbol, probeSymbol);
-
-    // If we schedule again but make the probe not a probe, it should be first.
-    when(() => probe.frame).thenReturn(otherFrame);
-    waiter
-      ..scheduleShip(probe, longAgo)
-      ..scheduleShip(other, recent);
-    expect(waiter.nextShip().shipSymbol, probeSymbol);
-    expect(waiter.nextShip().shipSymbol, otherSymbol);
-  });
 }
