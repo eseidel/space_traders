53624deea625cdc6ee46a788089fdeb2d225eced
Eric Seidel <github@eseidel.com>
2023-10-15 19:29:28 -0700

    test: fix tests after previous commit

--- packages/cli/test/behavior/central_command_test.dart
+++ packages/cli/test/behavior/central_command_test.dart
@@ -453,6 +453,10 @@ void main() {
     when(() => caches.marketPrices.prices).thenReturn([]);
     registerFallbackValue(TradeSymbol.ADVANCED_CIRCUITRY);
     when(() => caches.marketPrices.havePriceFor(any())).thenReturn(false);
+    when(() => caches.ships.countOfType(ShipType.ORE_HOUND)).thenReturn(0);
+    when(() => caches.ships.countOfType(ShipType.HEAVY_FREIGHTER))
+        .thenReturn(0);
+    when(() => caches.ships.countOfType(ShipType.PROBE)).thenReturn(0);
 
     await centralCommand.advanceCentralPlanning(api, caches);
     expect(centralCommand.nextShipBuyJob, isNull);
