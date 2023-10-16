767e5f332a84f646f1b9b67193be8a4f0134f0d1
Eric Seidel <github@eseidel.com>
2023-10-15 20:36:27 -0700

    feat: list what's missing from static cache

---
+++ packages/cli/bin/static_cache_missing.dart
@@ -0,0 +1,30 @@
+import 'package:cli/cache/static_cache.dart';
+import 'package:cli/cli.dart';
+
+void _printMissing<K extends Object, V extends Object>(
+  Iterable<K> symbols,
+  StaticCache<K, V> cache,
+) {
+  final missingSymbols = symbols.where((s) => cache[s] == null).toList();
+  if (missingSymbols.isNotEmpty) {
+    logger.info('${missingSymbols.length} missing $K:');
+    for (final symbol in missingSymbols) {
+      logger.info('  $symbol');
+    }
+  }
+}
+
+Future<void> command(FileSystem fs, ArgResults argResults) async {
+  final staticCaches = StaticCaches.load(fs);
+
+  _printMissing(ShipMountSymbolEnum.values, staticCaches.mounts);
+  _printMissing(ShipModuleSymbolEnum.values, staticCaches.modules);
+  _printMissing(ShipType.values, staticCaches.shipyardShips);
+  _printMissing(ShipEngineSymbolEnum.values, staticCaches.engines);
+  _printMissing(ShipReactorSymbolEnum.values, staticCaches.reactors);
+  _printMissing(WaypointTraitSymbolEnum.values, staticCaches.waypointTraits);
+}
+
+void main(List<String> args) async {
+  await runOffline(args, command);
+}
