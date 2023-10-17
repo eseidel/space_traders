ad3e705074c8669996757b9753f51e711a15afbb
Eric Seidel <github@eseidel.com>
2023-10-16 20:39:14 -0700

    feat: print waypoint trait counts

--- packages/cli/bin/waypoints.dart
+++ packages/cli/bin/waypoints.dart
@@ -5,6 +5,19 @@ Future<void> command(FileSystem fs, ArgResults argResults) async {
   final chartingCache = ChartingCache.load(fs);
   final waypointCount = chartingCache.waypointCount;
   logger.info('Waypoint count: $waypointCount');
+
+  final traitCounts = <String, int>{};
+  for (final waypoint in chartingCache.waypoints) {
+    for (final trait in waypoint.traits) {
+      traitCounts[trait] = (traitCounts[trait] ?? 0) + 1;
+    }
+  }
+  for (final trait in WaypointTraitSymbolEnum.values) {
+    final count = traitCounts[trait.value];
+    if (count != null) {
+      logger.info('Waypoint trait ${trait.value}: $count');
+    }
+  }
 }
 
 void main(List<String> args) async {
