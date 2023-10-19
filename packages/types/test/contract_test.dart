cbcf177718d6b3174dc6458df531055646d2c59f
Eric Seidel <github@eseidel.com>
2023-10-18 23:19:06 -0700

    test: add test for ContractTransaction

---
+++ packages/types/test/contract_test.dart
@@ -0,0 +1,48 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+import 'package:types/types.dart';
+
+class _MockContract extends Mock implements Contract {}
+
+class _MockContractTerms extends Mock implements ContractTerms {}
+
+void main() {
+  test('Contract types', () {
+    final contract = _MockContract();
+    when(() => contract.id).thenReturn('id');
+    final contractTerms = _MockContractTerms();
+    when(() => contract.terms).thenReturn(contractTerms);
+    when(() => contractTerms.payment)
+        .thenReturn(ContractPayment(onAccepted: 10, onFulfilled: 20));
+
+    const shipSymbol = ShipSymbol('S', 1);
+    final waypointSymbol = WaypointSymbol.fromString('S-A-B');
+    final timestamp = DateTime(2021);
+
+    final accept = ContractTransaction.accept(
+      contract: contract,
+      shipSymbol: shipSymbol,
+      waypointSymbol: waypointSymbol,
+      timestamp: timestamp,
+    );
+    expect(accept.contractAction, ContractAction.accept);
+    expect(accept.creditsChange, 10);
+    final deliver = ContractTransaction.delivery(
+      contract: contract,
+      shipSymbol: shipSymbol,
+      waypointSymbol: waypointSymbol,
+      timestamp: timestamp,
+      unitsDelivered: 10,
+    );
+    expect(deliver.creditsChange, 0);
+    expect(deliver.contractAction, ContractAction.delivery);
+    final fulfill = ContractTransaction.fulfillment(
+      contract: contract,
+      shipSymbol: shipSymbol,
+      waypointSymbol: waypointSymbol,
+      timestamp: timestamp,
+    );
+    expect(fulfill.contractAction, ContractAction.fulfillment);
+    expect(fulfill.creditsChange, 20);
+  });
+}
