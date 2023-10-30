72cc1b1c84a90e66ca936f44cd95f1bf458a8754
Eric Seidel <github@eseidel.com>
2023-10-30 10:26:19 +0000

    fix: avoid ships < 100 fuel capacity running out.

--- packages/types/lib/api.dart
+++ packages/types/lib/api.dart
@@ -385,7 +385,15 @@ extension ShipUtils on Ship {
   /// Returns true if the ship should refuel.
   // One fuel bought from the market is 100 units of fuel in the ship.
   // For repeated short trips, avoiding buying fuel when we're close to full.
-  bool get shouldRefuel => fuel.current < (fuel.capacity - 100);
+  bool get shouldRefuel {
+    if (fuel.capacity == 0) {
+      return false;
+    }
+    if (fuel.capacity < 100) {
+      return fuel.current < fuel.capacity;
+    }
+    return fuel.current < (fuel.capacity - 100);
+  }
 
   /// Returns the number of units of fuel needed to top up the ship.
   /// This is in ship fuel units, not market fuel units.
