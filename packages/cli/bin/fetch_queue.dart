dff4a2d7a16be1fdd1ed05925ebb183c5e2799ae
Eric Seidel <github@eseidel.com>
2023-11-04 20:38:35 +0000

    refactor: make fetch_queue more generic

--- packages/cli/bin/fetch_queue.dart
+++ packages/cli/bin/fetch_queue.dart
@@ -3,31 +3,50 @@ import 'package:cli/cli.dart';
 import 'package:cli/net/auth.dart';
 import 'package:cli/net/queries.dart';
 
+class FetchQueue {
+  FetchQueue(this.api, this.db, this.caches);
+
+  final Api api;
+  final Database db;
+  final Caches caches;
+  final List<SystemSymbol> _systems = [];
+
+  void queueSystem(SystemSymbol systemSymbol) {
+    _systems.add(systemSymbol);
+  }
+
+  Future<void> processSystem(SystemSymbol systemSymbol) async {
+    final waypoints = await caches.waypoints.waypointsInSystem(systemSymbol);
+    for (final waypoint in waypoints) {
+      if (waypoint.hasMarketplace) {
+        final listing = caches.marketListings
+            .marketListingForSymbol(waypoint.waypointSymbol);
+        if (listing == null) {
+          final market = await getMarket(api, waypoint);
+          caches.marketListings.addMarket(market);
+        }
+      }
+    }
+  }
+
+  Future<void> run() async {
+    while (_systems.isNotEmpty) {
+      if (_systems.isNotEmpty) {
+        final system = _systems.removeLast();
+        await processSystem(system);
+      }
+    }
+  }
+}
+
 Future<void> command(FileSystem fs, ArgResults argResults) async {
   final db = await defaultDatabase();
   final api = defaultApi(fs, db, getPriority: () => 0);
-  final systems = await SystemsCache.load(fs);
-  final staticCaches = StaticCaches.load(fs);
-  final charting = ChartingCache.load(fs, staticCaches.waypointTraits);
-  final construction = ConstructionCache.load(fs);
-  final waypointCache = WaypointCache(api, systems, charting, construction);
-  final agentCache = AgentCache.loadCached(fs)!;
-  final marketListings = MarketListingCache.load(fs, staticCaches.tradeGoods);
+  final caches = await Caches.load(fs, api, db);
 
-  final systemSymbol = agentCache.agent.headquartersSymbol.systemSymbol;
-  final waypoints = await waypointCache.waypointsInSystem(systemSymbol);
-  final marketWaypoints =
-      waypoints.where((waypoint) => waypoint.hasMarketplace);
-  logger.info('Found ${marketWaypoints.length} market waypoints');
-  for (final waypoint in marketWaypoints) {
-    if (marketListings.marketListingForSymbol(waypoint.waypointSymbol) !=
-        null) {
-      continue;
-    }
-    final market = await getMarket(api, waypoint);
-    logger.info('Adding market for ${waypoint.waypointSymbol}');
-    marketListings.addMarket(market);
-  }
+  final systemSymbol = caches.agent.agent.headquartersSymbol.systemSymbol;
+  final queue = FetchQueue(api, db, caches)..queueSystem(systemSymbol);
+  await queue.run();
 
   // required or main() will hang
   await db.close();
