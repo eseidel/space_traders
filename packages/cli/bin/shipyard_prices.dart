d6f04d5d8763b022504277ae43079c8740faa312
Eric Seidel <github@eseidel.com>
2023-11-18 21:01:06 +0000

    feat: improve shipyard_prices output

--- packages/cli/bin/shipyard_prices.dart
+++ packages/cli/bin/shipyard_prices.dart
@@ -1,32 +1,61 @@
-import 'dart:math';
-
 import 'package:cli/cache/shipyard_prices.dart';
+import 'package:cli/cache/static_cache.dart';
 import 'package:cli/cli.dart';
 import 'package:cli/printing.dart';
+import 'package:cli_table/cli_table.dart';
 
 Future<void> command(FileSystem fs, ArgResults argResults) async {
   final shipyardPrices = ShipyardPrices.load(fs);
+  final shipyardShips = ShipyardShipCache.load(fs);
+  final showAll = argResults['all'] as bool;
 
   logger.info(
     'Loaded ${shipyardPrices.count} prices from '
     '${shipyardPrices.waypointCount} waypoints.',
   );
 
-  final maxNameLength =
-      ShipType.values.fold(0, (m, t) => max(m, t.value.length));
+  final table = Table(
+    header: [
+      'Type',
+      'Price',
+      'Cargo',
+      'Fuel',
+      'Speed',
+      'Mount Points',
+    ],
+  );
 
-  // Load up the ShipyardPrices.
-  // Walk through all ship types.
-  // Print median price for each.
   for (final shipType in ShipType.values) {
     final medianPrice = shipyardPrices.medianPurchasePrice(shipType);
-    final priceString =
-        (medianPrice == null ? '' : creditsString(medianPrice)).padLeft(13);
+    if (medianPrice == null && !showAll) {
+      continue;
+    }
     final name = shipType.value.substring('SHIP_'.length);
-    logger.info('${name.padRight(maxNameLength)} $priceString');
+    final ship = shipyardShips[shipType];
+    table.add([
+      name,
+      if (medianPrice == null) '' else creditsString(medianPrice),
+      ship?.cargoCapacity,
+      ship?.frame.fuelCapacity,
+      ship?.engine.speed,
+      ship?.frame.mountingPoints,
+    ]);
   }
+  logger.info(table.toString());
 }
 
 void main(List<String> args) async {
-  await runOffline(args, command);
+  await runOffline(
+    args,
+    command,
+    addArgs: (parser) {
+      parser.addFlag(
+        'all',
+        abbr: 'a',
+        negatable: false,
+        help:
+            'Print all ship types, including those not available for purchase.',
+      );
+    },
+  );
 }
