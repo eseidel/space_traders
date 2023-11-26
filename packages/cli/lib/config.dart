a617956fd10483462189519916176f2fce7ea251
Eric Seidel <github@eseidel.com>
2023-11-26 21:57:36 +0000

    chore: add more haulers

--- packages/cli/lib/config.dart
+++ packages/cli/lib/config.dart
@@ -18,20 +18,7 @@ class Config {
     ShipType.SURVEYOR,
     ShipType.MINING_DRONE,
     ShipType.SURVEYOR,
-    ShipType.LIGHT_HAULER,
-    ShipType.LIGHT_HAULER,
-    ShipType.LIGHT_HAULER,
-    ShipType.LIGHT_HAULER,
-    ShipType.LIGHT_HAULER,
-    ShipType.LIGHT_HAULER,
-    ShipType.LIGHT_HAULER,
-    ShipType.LIGHT_HAULER,
-    ShipType.LIGHT_HAULER,
-    ShipType.LIGHT_HAULER,
-    ShipType.LIGHT_HAULER,
-    ShipType.LIGHT_HAULER,
-    ShipType.LIGHT_HAULER,
-    ShipType.LIGHT_HAULER,
+    for (int i = 0; i < 15; i++) ShipType.LIGHT_HAULER,
   ];
 
   /// A list of which haulers should be used as miner haulers.
