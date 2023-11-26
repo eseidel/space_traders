fd8ee3bf6b325dff2bb16c688839616da21baf00
Eric Seidel <github@eseidel.com>
2023-11-26 13:27:08 -0800

    Fix jsonMatches to support non-json pimitives

--- packages/cli/lib/compare.dart
+++ packages/cli/lib/compare.dart
@@ -1,9 +1,15 @@
+import 'dart:convert';
+
 import 'package:cli/logger.dart';
 import 'package:json_diff/json_diff.dart';
 
 /// Compare two objects via json encoding.
 bool jsonMatches<T extends Object>(T actual, T expected) {
-  final differ = JsonDiffer.fromJson(actual, expected);
+  // Encode both objects to json to avoid cases where the objects are
+  // different but the json is the same.
+  final actualString = jsonEncode(actual);
+  final expectedString = jsonEncode(expected);
+  final differ = JsonDiffer(actualString, expectedString);
   final diff = differ.diff();
   if (diff.hasNothing) {
     return true;
--- packages/cli/test/compare_test.dart
+++ packages/cli/test/compare_test.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:cli/compare.dart';
 import 'package:cli/logger.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
+import 'package:types/types.dart';
 
 class _MockLogger extends Mock implements Logger {}
 
@@ -47,5 +50,16 @@ void main() {
         '+ {"a":1,"b":2}',
       ),
     ).called(1);
+  });
+  test('jsonMatches non-json objects', () {
+    const json =
+        '{"symbol":"MOUNT_GAS_SIPHON_I","name":"Gas Siphon I","description":"A basic gas siphon that can extract gas from gas giants and other gas-rich bodies.","strength":10,"deposits":[],"requirements":{"power":1,"crew":0,"slots":null}}';
+    final a = ShipMount.fromJson(jsonDecode(json))!;
+    final b = ShipMount.fromJson(jsonDecode(json))!;
+
+    final logger = Logger();
+    runWithLogger(logger, () {
+      expect(jsonMatches(a, b), isTrue);
+    });
   });
 }
