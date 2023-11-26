6634875b5f4f148a174270dbbd83b09a6e09da9b
Eric Seidel <github@eseidel.com>
2023-11-25 20:05:26 -0800

    test: add cli_test.dart

---
+++ packages/cli/test/cli_test.dart
@@ -0,0 +1,25 @@
+import 'package:cli/cli.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+class _MockLogger extends Mock implements Logger {}
+
+void main() {
+  test('runOffline', () {
+    final logger = _MockLogger();
+    runOffline(
+      ['-v'],
+      (fs, results) async {
+        expect(results['verbose'], true);
+        expect(results['help'], false);
+      },
+      overrideLogger: logger,
+    );
+    verify(() => logger.level = Level.verbose).called(1);
+  });
+
+  test('shipTypeFromArg, argFromShipType', () {
+    expect(shipTypeFromArg('COMMAND_FRIGATE'), ShipType.COMMAND_FRIGATE);
+    expect(argFromShipType(ShipType.COMMAND_FRIGATE), 'COMMAND_FRIGATE');
+  });
+}
