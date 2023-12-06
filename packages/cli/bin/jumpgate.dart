9218193dcdf55f8bf6d93d06996c4f92af64b9cc
Eric Seidel <github@eseidel.com>
2023-12-06 05:25:19 +0000

    feat: add a script to list jumpgate connections

---
+++ packages/cli/bin/jumpgate.dart
@@ -0,0 +1,42 @@
+import 'package:cli/cache/caches.dart';
+import 'package:cli/cli.dart';
+import 'package:cli/net/auth.dart';
+import 'package:cli/net/queries.dart';
+
+Future<void> command(FileSystem fs, ArgResults argResults) async {
+  // Load up the jump gate for the main system.
+  // List its connections.
+  // Check if each is constructed.
+  final db = await defaultDatabase();
+  final api = defaultApi(fs, db, getPriority: () => -1);
+
+  final agentCache = AgentCache.load(fs)!;
+  final hqSystem = agentCache.headquartersSystemSymbol;
+  final systemsCache = SystemsCache.load(fs)!;
+  final jumpGateSymbol = systemsCache
+      .waypointsInSystem(hqSystem)
+      .firstWhere((w) => w.isJumpGate)
+      .waypointSymbol;
+  final staticCaches = StaticCaches.load(fs);
+
+  final chartingCache = ChartingCache.load(fs, staticCaches.waypointTraits);
+  final constructionCache = ConstructionCache.load(fs);
+  final waypointCache =
+      WaypointCache(api, systemsCache, chartingCache, constructionCache);
+  final jumpGate = await getJumpGate(api, jumpGateSymbol);
+  logger.info('$jumpGateSymbol:');
+  for (final connection in jumpGate.connections) {
+    final symbol = WaypointSymbol.fromString(connection);
+    final waypoint = await waypointCache.waypoint(symbol);
+    final status =
+        waypoint.isUnderConstruction ? 'under construction' : 'ready';
+    logger.info('  ${symbol.sectorLocalName.padRight(9)} $status');
+  }
+
+  // Required or main() will hang.
+  await db.close();
+}
+
+void main(List<String> args) async {
+  await runOffline(args, command);
+}
