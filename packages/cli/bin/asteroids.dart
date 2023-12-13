8cf32c567c4c229762793ce984b08b52744760e8
Eric Seidel <github@eseidel.com>
2023-12-13 02:01:26 +0000

    feat: add a script to list asteroids with markets
    There are none.

---
+++ packages/cli/bin/asteroids.dart
@@ -0,0 +1,29 @@
+import 'package:cli/cache/caches.dart';
+import 'package:cli/cli.dart';
+
+Future<void> command(FileSystem fs, ArgResults argResults) async {
+  // List all known asteroids that have a market or shipyard.
+  final staticCaches = StaticCaches.load(fs);
+  final marketListingsCache =
+      MarketListingCache.load(fs, staticCaches.tradeGoods);
+  final shipyardListingCache = ShipyardListingCache.load(fs);
+  final systemsCache = SystemsCache.load(fs)!;
+
+  for (final marketListing in marketListingsCache.listings) {
+    final waypointSymbol = marketListing.waypointSymbol;
+    final waypoint = systemsCache.waypoint(waypointSymbol);
+    if (!waypoint.isAsteroid) {
+      continue;
+    }
+    final shipyardListing = shipyardListingCache[waypointSymbol];
+    if (shipyardListing != null) {
+      logger.info('Asteroid $waypointSymbol has a market and shipyard.');
+    } else {
+      logger.info('Asteroid $waypointSymbol has a market.');
+    }
+  }
+}
+
+void main(List<String> args) async {
+  await runOffline(args, command);
+}
