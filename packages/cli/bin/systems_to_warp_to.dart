70b748c1f02b7cf6a182bfa83e03781aa6e8d325
Eric Seidel <github@eseidel.com>
2023-12-16 10:38:03 -0800

    fix: make ChartingCache backwards compatible

--- packages/cli/bin/systems_to_warp_to.dart
+++ packages/cli/bin/systems_to_warp_to.dart
@@ -1,13 +1,19 @@
 import 'package:cli/cache/caches.dart';
 import 'package:cli/cli.dart';
+import 'package:cli/net/auth.dart';
 import 'package:collection/collection.dart';
 
 // List the 10 nearest systems which have 10+ markets and are not reachable
 // via jumpgates from HQ.  Systems worth warping to should be charted already.
 Future<void> command(FileSystem fs, ArgResults argResults) async {
+  const limit = 10;
+  const desiredMarketCount = 10;
+
+  final db = await defaultDatabase();
+  final api = defaultApi(fs, db, getPriority: () => networkPriorityLow);
+
   final agentCache = AgentCache.load(fs)!;
   final startSystemSymbol = agentCache.headquartersSystemSymbol;
-  const limit = 10;
 
   final staticCaches = StaticCaches.load(fs);
   final jumpGateCache = JumpGateCache.load(fs);
@@ -16,6 +22,9 @@ Future<void> command(FileSystem fs, ArgResults argResults) async {
       SystemConnectivity.fromJumpGates(jumpGateCache, constructionCache);
   final systemsCache = SystemsCache.load(fs)!;
   final chartingCache = ChartingCache.load(fs, staticCaches.waypointTraits);
+
+  final waypointCache =
+      WaypointCache(api, systemsCache, chartingCache, constructionCache);
 
   final reachableSystemSymbols =
       systemConnectivity.systemsReachableFrom(startSystemSymbol).toSet();
@@ -27,7 +36,26 @@ Future<void> command(FileSystem fs, ArgResults argResults) async {
       .sortedBy<num>((s) => s.distanceTo(startSystem))
       .where((s) => !reachableSystemSymbols.contains(s.systemSymbol));
 
-  // Fetch waypoints?.  Really should keep some sort of negative cache.
+  final systemsToWarpTo = <System>[];
+  for (final system in systemsByDistance) {
+    if (systemsToWarpTo.length >= limit) {
+      break;
+    }
+    final waypoints =
+        await waypointCache.waypointsInSystem(system.systemSymbol);
+    final marketCount = waypoints.where((w) => w.hasMarketplace).length;
+    if (marketCount < desiredMarketCount) {
+      continue;
+    }
+    systemsToWarpTo.add(system);
+  }
+
+  for (final system in systemsToWarpTo) {
+    logger.info('${system.systemSymbol}');
+  }
+
+  // Required or main will hang.
+  await db.close();
 }
 
 void main(List<String> args) async {
