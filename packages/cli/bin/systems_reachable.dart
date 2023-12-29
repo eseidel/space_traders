506319c711f1995545962f0d9cf3f2b56094dc8a
Eric Seidel <github@eseidel.com>
2023-12-29 21:25:01 +0000

    feat: dump more stats in systems_reachable.dart

--- packages/cli/bin/systems_reachable.dart
+++ packages/cli/bin/systems_reachable.dart
@@ -11,7 +11,8 @@ Future<void> command(FileSystem fs, ArgResults argResults) async {
     startSystemSymbol = agentCache.headquartersSystemSymbol;
   }
 
-  final sytemsCache = SystemsCache.load(fs)!;
+  logger.info('Starting from $startSystemSymbol, known reachable:');
+  final systemsCache = SystemsCache.load(fs)!;
   final jumpGateCache = JumpGateCache.load(fs);
   final constructionSnapshot = await ConstructionSnapshot.load(db);
   final systemConnectivity =
@@ -19,18 +20,19 @@ Future<void> command(FileSystem fs, ArgResults argResults) async {
 
   final reachableSystems =
       systemConnectivity.systemsReachableFrom(startSystemSymbol).toSet();
-  logger.info('${reachableSystems.length} reachable systems');
+  logger.info('${reachableSystems.length} systems');
 
   var waypointCount = 0;
   for (final systemSymbol in reachableSystems) {
-    final systemRecord = sytemsCache[systemSymbol];
+    final systemRecord = systemsCache[systemSymbol];
     waypointCount += systemRecord.waypoints.length;
   }
-  logger.info('containing $waypointCount waypoints');
+  logger.info('$waypointCount waypoints');
 
   // How many waypoints are charted?
   final chartingSnapshot = await ChartingSnapshot.load(db);
   var chartedWaypoints = 0;
+  var chartedJumpGates = 0;
   for (final record in chartingSnapshot.records) {
     if (!record.isCharted) {
       continue;
@@ -40,8 +42,13 @@ Future<void> command(FileSystem fs, ArgResults argResults) async {
     if (reachableSystems.contains(systemSymbol)) {
       chartedWaypoints += 1;
     }
+    final waypoint = systemsCache.waypoint(record.waypointSymbol);
+    if (waypoint.isJumpGate) {
+      chartedJumpGates += 1;
+    }
   }
-  logger.info('$chartedWaypoints charted waypoints');
+  logger.info(' $chartedWaypoints charted');
+
   // How many markets?
   final tradeGoodCache = TradeGoodCache.load(fs);
   final marketListingCache = MarketListingCache.load(fs, tradeGoodCache);
@@ -54,6 +61,7 @@ Future<void> command(FileSystem fs, ArgResults argResults) async {
     }
   }
   logger.info('$markets markets');
+
   // How many shipyards?
   final shipyardListingCache = ShipyardListingCache.load(fs);
   var shipyards = 0;
@@ -65,7 +73,28 @@ Future<void> command(FileSystem fs, ArgResults argResults) async {
     }
   }
   logger.info('$shipyards shipyards');
+
   // How many total jump gates (presumably the exact number of systems?)
+  var jumpGates = 0;
+  for (final systemSymbol in reachableSystems) {
+    final systemRecord = systemsCache[systemSymbol];
+    jumpGates += systemRecord.jumpGateWaypoints.length;
+  }
+  logger.info('$jumpGates jump gates');
+
+  // How many are cached?
+  var cachedJumpGates = 0;
+  for (final record in jumpGateCache.values) {
+    // We could sort first by system to save ourselves some lookups.
+    final systemSymbol = record.waypointSymbol.system;
+    if (reachableSystems.contains(systemSymbol)) {
+      cachedJumpGates += 1;
+    }
+  }
+  logger
+    ..info(' $cachedJumpGates cached')
+    ..info(' $chartedJumpGates charted');
+
   // How many blocked connections?
   // To how many unique blocked endpoints?
 
