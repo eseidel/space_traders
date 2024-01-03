986a469d093d33743e0f03e808ac3d6877f0fce1
Eric Seidel <github@eseidel.com>
2024-01-03 06:55:40 +0000

    chore: buy probes

--- packages/cli/lib/config.dart
+++ packages/cli/lib/config.dart
@@ -34,7 +34,7 @@ class Config {
     ShipType.MINING_DRONE,
     ShipType.SURVEYOR,
     for (int i = 0; i < 15; i++) ShipType.LIGHT_HAULER,
-    // for (int i = 0; i < 40; i++) ShipType.PROBE,
+    for (int i = 0; i < 40; i++) ShipType.PROBE,
     // for (int i = 0; i < 10; i++) ShipType.REFINING_FREIGHTER,
     // ShipType.EXPLORER,
   ];
