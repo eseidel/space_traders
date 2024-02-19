2fe60ade33180c70af32e3d5d516f8f1ce9871eb
Eric Seidel <github@eseidel.com>
2024-02-19 11:52:04 -0800

    chore: add db reset script

---
+++ packages/db/tools/reset.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd $SCRIPT_DIR
+cd ..
+docker cp scripts/. spacetraders_postgres:/scripts
+docker cp sql/. spacetraders_postgres:/sql
+docker exec spacetraders_postgres /scripts/init_db.sh spacetraders
