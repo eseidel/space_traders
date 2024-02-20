f341338bcdb5a69d74a93bf45323211b611555b3
Eric Seidel <github@eseidel.com>
2024-02-20 02:37:02 +0000

    feat: add a linux setup script

---
+++ scripts/linux_setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+snap install flutter --classic
+snap install docker
+
+cd packages/db
+docker pull postgres
+docker run \
+    --name spacetraders_postgres \
+    -p 127.0.0.1:5432:5432 \
+    -e POSTGRES_USER=postgres \
+    -e POSTGRES_DB=spacetraders \
+    -e POSTGRES_PASSWORD=password \
+    -d \
+    postgres
+
+docker cp scripts/. spacetraders_postgres:/scripts
+docker cp sql/. spacetraders_postgres:/sql
+
+docker exec spacetraders_postgres /scripts/init_db.sh spacetraders
