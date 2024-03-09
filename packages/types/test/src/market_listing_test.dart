bc31f2a3d2e03effa88fb6e8b856aa07f440bda1
Eric Seidel <github@eseidel.com>
2024-03-08 20:47:35 -0800

    test: add listing tests

--- packages/types/test/src/market_listing_test.dart
+++ packages/types/test/src/market_listing_test.dart
@@ -10,4 +10,14 @@ void main() {
     expect(market.allowsTradeOf(TradeSymbol.FABRICS), isFalse);
     expect(market.allowsTradeOf(TradeSymbol.FUEL), isTrue);
   });
+
+  test('MarketListing json round trip', () {
+    final market = MarketListing(
+      waypointSymbol: WaypointSymbol.fromString('S-A-W'),
+      exports: const {TradeSymbol.FUEL},
+    );
+    final json = market.toJson();
+    final market2 = MarketListing.fromJson(json);
+    expect(market, market2);
+  });
 }
---
+++ packages/types/test/src/shipyard_listing_test.dart
@@ -0,0 +1,23 @@
+import 'package:test/test.dart';
+import 'package:types/types.dart';
+
+void main() {
+  test('ShipyardListing.allowsTradeOf', () {
+    final listing = ShipyardListing(
+      waypointSymbol: WaypointSymbol.fromString('S-A-W'),
+      shipTypes: const {ShipType.COMMAND_FRIGATE},
+    );
+    expect(listing.hasShip(ShipType.EXPLORER), isFalse);
+    expect(listing.hasShip(ShipType.COMMAND_FRIGATE), isTrue);
+  });
+
+  test('ShipyardListing json round trip', () {
+    final listing = ShipyardListing(
+      waypointSymbol: WaypointSymbol.fromString('S-A-W'),
+      shipTypes: const {ShipType.COMMAND_FRIGATE},
+    );
+    final json = listing.toJson();
+    final listing2 = ShipyardListing.fromJson(json);
+    expect(listing, listing2);
+  });
+}
