3897a95db03b3d551d5f3619e7a01f6544529e13
Eric Seidel <github@eseidel.com>
2024-03-08 20:37:31 -0800

    test: add basic JumpGate test

---
+++ packages/types/test/src/jump_gate_test.dart
@@ -0,0 +1,22 @@
+import 'package:test/test.dart';
+import 'package:types/types.dart';
+
+void main() {
+  test('JumpGate', () {
+    final waypointSymbol = WaypointSymbol.fromString('S-A-B');
+    final connections = {
+      WaypointSymbol.fromString('S-A-A'),
+      WaypointSymbol.fromString('S-B-C'),
+    };
+    final jumpGate =
+        JumpGate(waypointSymbol: waypointSymbol, connections: connections);
+    expect(jumpGate.connectedSystemSymbols, {
+      SystemSymbol.fromString('S-A'),
+      SystemSymbol.fromString('S-B'),
+    });
+
+    final json = jumpGate.toJson();
+    final fromJson = JumpGate.fromJson(json);
+    expect(fromJson, jumpGate);
+  });
+}
