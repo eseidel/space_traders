bec26fdd09c38fe96805837efe671e66c37ef25f
Eric Seidel <github@eseidel.com>
2024-03-09 14:55:48 -0800

    refactor: move tests for custom types into correct files

---
+++ packages/types/test/src/symbol_test.dart
@@ -0,0 +1,74 @@
+import 'package:test/test.dart';
+import 'package:types/types.dart';
+
+void main() {
+  test('SystemSymbol equality', () {
+    final a = SystemSymbol.fromString('S-E');
+    final b = SystemSymbol.fromString('S-E');
+    final c = SystemSymbol.fromString('S-Q');
+    expect(a, b);
+    expect(a, isNot(c));
+    expect(a.sector, 'S');
+
+    expect(() => SystemSymbol.fromString('S'), throwsArgumentError);
+    expect(() => SystemSymbol.fromString('S-E-A'), throwsArgumentError);
+  });
+  test('WaypointSymbol equality', () {
+    final a = WaypointSymbol.fromString('S-E-J');
+    final b = WaypointSymbol.fromString('S-E-J');
+    final c = WaypointSymbol.fromString('S-E-P');
+    expect(a, b);
+    expect(a, isNot(c));
+    expect(a.sector, 'S');
+    expect(a.system, SystemSymbol.fromString('S-E'));
+    expect(a.systemString, 'S-E');
+
+    expect(() => WaypointSymbol.fromString('S-E'), throwsArgumentError);
+    expect(() => WaypointSymbol.fromString('S-E-A-F'), throwsArgumentError);
+  });
+  test('WaypointPosition distance', () {
+    final system = SystemSymbol.fromString('S-E');
+    final a = WaypointPosition(0, 0, system);
+    final b = WaypointPosition(3, 4, system);
+    expect(a.distanceTo(b), 5);
+    expect(b.distanceTo(a), 5);
+    final c = WaypointPosition(3, 0, SystemSymbol.fromString('S-F'));
+    expect(() => a.distanceTo(c), throwsArgumentError);
+  });
+  test('WaypointSymbol.fromJsonOrNull', () {
+    final symbol = WaypointSymbol.fromJsonOrNull('S-E-J');
+    expect(symbol, WaypointSymbol.fromJsonOrNull('S-E-J'));
+    expect(WaypointSymbol.fromJsonOrNull(null), isNull);
+
+    // Invalid still throws.
+    expect(() => WaypointSymbol.fromJsonOrNull('S-E'), throwsArgumentError);
+    expect(() => WaypointSymbol.fromJsonOrNull('S-E-A-F'), throwsArgumentError);
+  });
+
+  test('WaypointSymbol.waypoinName and localSectorName', () {
+    final symbol = WaypointSymbol.fromString('S-E-J');
+    expect(symbol.waypointName, 'J');
+    expect(symbol.sectorLocalName, 'E-J');
+  });
+  test('ShipSymbol sorting', () {
+    final symbols = [
+      ShipSymbol.fromString('A-1A'),
+      ShipSymbol.fromString('A-A'),
+      ShipSymbol.fromString('A-2'),
+      const ShipSymbol('A', 1),
+    ]..sort();
+    expect(symbols, [
+      const ShipSymbol('A', 1),
+      ShipSymbol.fromString('A-2'),
+      ShipSymbol.fromString('A-A'),
+      ShipSymbol.fromString('A-1A'),
+    ]);
+  });
+  test('ShipSymbol agentName with hyphen', () {
+    final symbol = ShipSymbol.fromString('A-1-A');
+    expect(symbol.agentName, 'A-1');
+
+    // At least one hyphen is required.
+    expect(() => ShipSymbol.fromString('A1'), throwsArgumentError);
+  });
+}
---
+++ packages/types/test/system_test.dart
@@ -0,0 +1,51 @@
+import 'package:test/test.dart';
+import 'package:types/types.dart';
+
+void main() {
+  test('SystemWaypoint.isType', () {
+    final jumpGate = SystemWaypoint.test(
+      WaypointSymbol.fromString('S-E-J'),
+      type: WaypointType.JUMP_GATE,
+    );
+    final planet = SystemWaypoint.test(
+      WaypointSymbol.fromString('S-E-P'),
+      type: WaypointType.PLANET,
+    );
+    final asteroid = SystemWaypoint.test(
+      WaypointSymbol.fromString('S-E-A'),
+    );
+    expect(jumpGate.isType(WaypointType.JUMP_GATE), isTrue);
+    expect(jumpGate.isJumpGate, isTrue);
+    expect(jumpGate.isType(WaypointType.PLANET), isFalse);
+    expect(asteroid.isAsteroid, isTrue);
+    expect(jumpGate.system, SystemSymbol.fromString('S-E'));
+
+    final system = System.test(
+      SystemSymbol.fromString('S-E'),
+      waypoints: [
+        jumpGate,
+        planet,
+        asteroid,
+      ],
+    );
+    expect(system.jumpGateWaypoints.first, jumpGate);
+  });
+
+  test('Waypoint.hasTrait', () {
+    final waypoint = Waypoint.test(
+      WaypointSymbol.fromString('S-E-J'),
+      type: WaypointType.JUMP_GATE,
+      traits: [
+        WaypointTrait(
+          symbol: WaypointTraitSymbol.ASH_CLOUDS,
+          name: '',
+          description: '',
+        ),
+      ],
+    );
+    expect(waypoint.hasTrait(WaypointTraitSymbol.ASH_CLOUDS), isTrue);
+    expect(waypoint.hasTrait(WaypointTraitSymbol.BARREN), isFalse);
+    final systemWaypoint = waypoint.toSystemWaypoint();
+    expect(systemWaypoint.symbol, waypoint.symbol);
+  });
+}
