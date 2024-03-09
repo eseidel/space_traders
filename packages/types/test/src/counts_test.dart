2f636e8a6264803bfe9f3ccc215eb731f34db5e6
Eric Seidel <github@eseidel.com>
2024-03-09 15:37:18 -0800

    test: captureTimeAndRequests

--- packages/types/test/src/counts_test.dart
+++ packages/types/test/src/counts_test.dart
@@ -21,4 +21,27 @@ void main() {
     final diff = after.diff(before);
     expect(diff.counts, {'/foo': 1});
   });
+
+  test('captureTimeAndRequests', () async {
+    final requestCounts = RequestCounts()..record('/before');
+    final queryCounts = QueryCounts()..record('SELECT * FROM before_');
+    final result = await captureTimeAndRequests(
+      requestCounts,
+      queryCounts,
+      () async {
+        requestCounts.record('/during');
+        queryCounts.record('SELECT * FROM during_');
+        return 42;
+      },
+      onComplete: (duration, requestCount, queryCounts) {
+        expect(requestCount, 1);
+        expect(queryCounts.total, 1);
+        expect(
+          queryCounts.counts,
+          {'SELECT * FROM during_': 1},
+        );
+      },
+    );
+    expect(result, 42);
+  });
 }
