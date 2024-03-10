1e62631fd6f32f08c346e8caef9132d5520d87f8
Eric Seidel <github@eseidel.com>
2024-03-10 15:29:11 +0000

    feat: move short_warps to the new warp planner

--- packages/cli/bin/short_warps.dart
+++ packages/cli/bin/short_warps.dart
@@ -1,70 +1,70 @@
 import 'package:cli/caches.dart';
 import 'package:cli/cli.dart';
 import 'package:cli/nav/navigation.dart';
-import 'package:collection/collection.dart';
+import 'package:cli/nav/warp_pathing.dart';
 
 void main(List<String> args) async {
   await runOffline(args, command);
 }
 
-RoutePlan? shortestPathTo(
-  SystemConnectivity systemConnectivity,
-  RoutePlanner routePlanner,
-  SystemsCache systemsCache,
-  SystemSymbol systemSymbol,
-  Ship ship,
-) {
-  final maxFuel = ship.frame.fuelCapacity;
-  final system = systemsCache[systemSymbol];
-  final nearbySystems = systemsCache.systems.where(
-    (s) =>
-        s.symbol != systemSymbol &&
-        systemConnectivity.existsJumpPathBetween(s.symbol, ship.systemSymbol) &&
-        s.distanceTo(system) < maxFuel,
-  );
-  final routes = <RoutePlan>[];
-  for (final nearbySystem in nearbySystems) {
-    // Figure out the time to route from current location to the jumpgate
-    // for nearbySystem.
-    final jumpGate = nearbySystem.jumpGateWaypoints.first;
-    final route = routePlanner.planRoute(
-      ship.shipSpec,
-      start: ship.waypointSymbol,
-      end: jumpGate.symbol,
-    );
-    if (route != null) {
-      routes.add(route);
-    }
-  }
-  if (routes.isEmpty) {
-    return null;
-  }
-  final plan = routes.sortedBy((r) => r.duration).first;
-  final actions = List<RouteAction>.from(plan.actions);
-  // Should pick something more central.
-  final end = system.jumpGateWaypoints.first;
-  final nearbySystem = systemsCache[actions.last.endSymbol.system];
-  final distance = nearbySystem.distanceTo(system);
-  final seconds = warpTimeByDistanceAndSpeed(
-    distance: distance,
-    shipSpeed: ship.shipSpec.speed,
-    flightMode: ShipNavFlightMode.CRUISE,
-  );
-  final fuel = fuelUsedByDistance(
-    distance,
-    ShipNavFlightMode.CRUISE,
-  );
-  actions.add(
-    RouteAction(
-      startSymbol: plan.endSymbol,
-      endSymbol: end.symbol,
-      type: RouteActionType.warpCruise,
-      seconds: seconds,
-      fuelUsed: fuel,
-    ),
-  );
-  return plan.copyWith(actions: actions);
-}
+// RoutePlan? shortestPathTo(
+//   SystemConnectivity systemConnectivity,
+//   RoutePlanner routePlanner,
+//   SystemsCache systemsCache,
+//   SystemSymbol systemSymbol,
+//   Ship ship,
+// ) {
+//   final maxFuel = ship.frame.fuelCapacity;
+//   final system = systemsCache[systemSymbol];
+//   final nearbySystems = systemsCache.systems.where(
+//     (s) =>
+//         s.symbol != systemSymbol &&
+//         systemConnectivity.existsJumpPathBetween(s.symbol, ship.systemSymbol) &&
+//         s.distanceTo(system) < maxFuel,
+//   );
+//   final routes = <RoutePlan>[];
+//   for (final nearbySystem in nearbySystems) {
+//     // Figure out the time to route from current location to the jumpgate
+//     // for nearbySystem.
+//     final jumpGate = nearbySystem.jumpGateWaypoints.first;
+//     final route = routePlanner.planRoute(
+//       ship.shipSpec,
+//       start: ship.waypointSymbol,
+//       end: jumpGate.symbol,
+//     );
+//     if (route != null) {
+//       routes.add(route);
+//     }
+//   }
+//   if (routes.isEmpty) {
+//     return null;
+//   }
+//   final plan = routes.sortedBy((r) => r.duration).first;
+//   final actions = List<RouteAction>.from(plan.actions);
+//   // Should pick something more central.
+//   final end = system.jumpGateWaypoints.first;
+//   final nearbySystem = systemsCache[actions.last.endSymbol.system];
+//   final distance = nearbySystem.distanceTo(system);
+//   final seconds = warpTimeByDistanceAndSpeed(
+//     distance: distance,
+//     shipSpeed: ship.shipSpec.speed,
+//     flightMode: ShipNavFlightMode.CRUISE,
+//   );
+//   final fuel = fuelUsedByDistance(
+//     distance,
+//     ShipNavFlightMode.CRUISE,
+//   );
+//   actions.add(
+//     RouteAction(
+//       startSymbol: plan.endSymbol,
+//       endSymbol: end.symbol,
+//       type: RouteActionType.warpCruise,
+//       seconds: seconds,
+//       fuelUsed: fuel,
+//     ),
+//   );
+//   return plan.copyWith(actions: actions);
+// }
 
 Future<void> command(FileSystem fs, Database db, ArgResults argResults) async {
   // Systems to visit:
@@ -79,17 +79,17 @@ Future<void> command(FileSystem fs, Database db, ArgResults argResults) async {
   final agentCache = await AgentCache.load(db);
   final hqSystem = agentCache!.headquartersSystemSymbol;
 
-  final routePlanner = RoutePlanner.fromSystemsCache(
-    systemsCache,
-    systemConnectivity,
-    sellsFuel: defaultSellsFuel(marketListings),
-  );
+  // final routePlanner = RoutePlanner.fromSystemsCache(
+  //   systemsCache,
+  //   systemConnectivity,
+  //   sellsFuel: defaultSellsFuel(marketListings),
+  // );
 
   final explorer = ships.ships.firstWhere((s) => s.isExplorer);
 
   final unreachableSystems = systemsToWatch
       .where(
-        (systemSymbol) => systemConnectivity.existsJumpPathBetween(
+        (systemSymbol) => !systemConnectivity.existsJumpPathBetween(
           systemSymbol,
           hqSystem,
         ),
@@ -98,20 +98,30 @@ Future<void> command(FileSystem fs, Database db, ArgResults argResults) async {
 
   // Look check all systems within 800 units of an unreachable system for
   // a reachable system.
-  const maxFuel = 800;
+  final shipSpec = explorer.shipSpec;
+  final maxFuel = shipSpec.fuelCapacity;
   logger
-    ..info('Unreachable systems within $maxFuel fuel of reachable systems.')
+    ..info(
+      'Systems disconnected from $hqSystem within $maxFuel fuel via warp',
+    )
     ..info('with travel time by explorer at ${explorer.waypointSymbol}');
 
   for (final systemSymbol in unreachableSystems) {
-    final plan = shortestPathTo(
-      systemConnectivity,
-      routePlanner,
+    final system = systemsCache[systemSymbol];
+    final actions = findRouteBetweenSystems(
       systemsCache,
-      systemSymbol,
-      explorer,
+      systemConnectivity,
+      explorer.shipSpec,
+      start: explorer.waypointSymbol,
+      end: system.waypoints.first.symbol,
+      sellsFuel: defaultSellsFuel(marketListings),
     );
-    if (plan != null) {
+    if (actions != null) {
+      final plan = RoutePlan(
+        fuelCapacity: shipSpec.fuelCapacity,
+        shipSpeed: shipSpec.speed,
+        actions: actions,
+      );
       logger.info(
         '  $systemSymbol: ${describeRoutePlan(plan)}',
       );
