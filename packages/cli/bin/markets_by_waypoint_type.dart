e8bce1437e5283be69a5839d67853b2d598aa40c
Eric Seidel <github@eseidel.com>
2024-03-21 03:29:04 -0700

    feat: add markets_by_waypoint_type.dart

---
+++ packages/cli/bin/markets_by_waypoint_type.dart
@@ -0,0 +1,51 @@
+import 'package:cli/cache/charting_cache.dart';
+import 'package:cli/cache/market_listing_snapshot.dart';
+import 'package:cli/cache/systems_cache.dart';
+import 'package:cli/cli.dart';
+import 'package:cli_table/cli_table.dart';
+
+Map<String, dynamic> rightAlign(Object? content) => <String, dynamic>{
+      'content': content.toString(),
+      'hAlign': HorizontalAlign.right,
+    };
+
+Future<void> command(FileSystem fs, Database db, ArgResults argResults) async {
+  final chartingSnapshot = await ChartingSnapshot.load(db);
+  final marketListings = await MarketListingSnapshot.load(db);
+  final systemsCache = SystemsCache.load(fs)!;
+
+  // Walk all charting records.
+  // Record the number of known waypoints by type.
+  // Record if it has a market.
+  final waypointsByType = <WaypointType, int>{};
+  final waypointsWithMarket = <WaypointType, int>{};
+  for (final record in chartingSnapshot.records) {
+    final waypoint = systemsCache.waypoint(record.waypointSymbol);
+    waypointsByType[waypoint.type] = (waypointsByType[waypoint.type] ?? 0) + 1;
+    if (marketListings[record.waypointSymbol] != null) {
+      waypointsWithMarket[waypoint.type] =
+          (waypointsWithMarket[waypoint.type] ?? 0) + 1;
+    }
+  }
+  // Print the results.
+  final table = Table(
+    header: ['Waypoint Type', '%', 'Count', 'Markets'],
+    style: const TableStyle(compact: true),
+  );
+  for (final type in waypointsByType.keys) {
+    final waypointCount = waypointsByType[type]!;
+    final marketCount = waypointsWithMarket[type] ?? 0;
+    final percentWithMarket = (marketCount / waypointCount * 100).round();
+    table.add([
+      type.value,
+      rightAlign(percentWithMarket),
+      rightAlign(waypointCount),
+      rightAlign(marketCount),
+    ]);
+  }
+  logger.info(table.toString());
+}
+
+void main(List<String> args) async {
+  await runOffline(args, command);
+}
