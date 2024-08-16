3d52d8fdb46879d4c424c5a8d72523f402dfd7c7
Eric Seidel <github@eseidel.com>
2024-08-15 20:45:37 -0700

    feat: add a fleet screen to ui

--- packages/ui/lib/route.dart
+++ packages/ui/lib/route.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:http/http.dart' as http;
+import 'package:types/types.dart';
 
 /// The route configuration.
 final GoRouter router = GoRouter(
@@ -7,7 +11,7 @@ final GoRouter router = GoRouter(
     GoRoute(
       path: '/',
       builder: (BuildContext context, GoRouterState state) {
-        return Text("Map");
+        return const FleetScreen();
       },
       routes: <RouteBase>[
         GoRoute(
@@ -20,6 +24,56 @@ final GoRouter router = GoRouter(
     ),
   ],
 );
+
+class FleetScreen extends StatefulWidget {
+  const FleetScreen({super.key});
+
+  @override
+  State<FleetScreen> createState() => _FleetScreenState();
+}
+
+final Uri baseUri = Uri.parse('http://localhost:8081');
+
+class _FleetScreenState extends State<FleetScreen> {
+  List<Ship> ships = <Ship>[];
+  bool loading = true;
+
+  Future<void> refresh() async {
+    final uri = baseUri.replace(path: '/ships');
+    print(uri);
+    final response = await http.get(uri);
+    final json = jsonDecode(response.body) as List<dynamic>;
+    final newShips = json
+        .map((dynamic item) => Ship.fromJson(item as Map<String, dynamic>))
+        .toList();
+    setState(() {
+      ships = newShips;
+      loading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    refresh();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Fleet')),
+      body: ListView.builder(
+        itemCount: ships.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text(ships[index].symbol.symbol),
+            subtitle: Text(ships[index].toString()),
+          );
+        },
+      ),
+    );
+  }
+}
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
