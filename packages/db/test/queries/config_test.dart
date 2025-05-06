27a80420c86821aa5c8cb942f85a91c90c0d8b63
Eric Seidel <github@eseidel.com>
2025-05-06 23:23:24 +0000

    test: add test for db get/set auth token

--- packages/db/test/queries/config_test.dart
+++ packages/db/test/queries/config_test.dart
@@ -5,17 +5,30 @@ import '../docker.dart';
 
 void main() {
   withPostgresServer('config', (server) {
-    test('get and set agent symbol', () async {
-      final endpoint = await server.endpoint();
-      final db = Database.testLive(
-        endpoint: endpoint,
-        connection: await server.newConnection(),
-      );
-      await db.migrateToLatestSchema();
-      expect(await db.getAgentSymbol(), isNull);
-      const agentSymbol = 'S';
-      await db.setAgentSymbol(agentSymbol);
-      expect(await db.getAgentSymbol(), equals(agentSymbol));
+    group('get and set', () {
+      late Database db;
+      setUpAll(() async {
+        final endpoint = await server.endpoint();
+        db = Database.testLive(
+          endpoint: endpoint,
+          connection: await server.newConnection(),
+        );
+        await db.migrateToLatestSchema();
+      });
+
+      test('agent symbol', () async {
+        expect(await db.getAgentSymbol(), isNull);
+        const agentSymbol = 'S';
+        await db.setAgentSymbol(agentSymbol);
+        expect(await db.getAgentSymbol(), equals(agentSymbol));
+      });
+
+      test('auth token', () async {
+        expect(await db.getAuthToken(), isNull);
+        const token = '1234567890';
+        await db.setAuthToken(token);
+        expect(await db.getAuthToken(), equals(token));
+      });
     });
   });
 }
