84fddf4d82452f888a89ce16d7a10488be7e3244
Eric Seidel <github@eseidel.com>
2025-05-09 04:07:16 +0000

    test: ShipyardPriceSnapshot.medianPurchasePrice

--- packages/types/test/src/snapshot/shipyard_price_snapshot_test.dart
+++ packages/types/test/src/snapshot/shipyard_price_snapshot_test.dart
@@ -2,8 +2,32 @@ import 'package:test/test.dart';
 import 'package:types/types.dart';
 
 void main() {
-  test('shipyard price snapshot', () {
-    final snapshot = ShipyardPriceSnapshot([]);
-    expect(snapshot.prices, isEmpty);
+  group('ShipyardPriceSnapshot', () {
+    test('constructor', () {
+      final snapshot = ShipyardPriceSnapshot([]);
+      expect(snapshot.prices, isEmpty);
+    });
+
+    test('medianPurchasePrice', () {
+      const shipType = ShipType.PROBE;
+      ShipyardPrice makePrice(String waypoint, int price) {
+        return ShipyardPrice(
+          waypointSymbol: WaypointSymbol.fromString(waypoint),
+          shipType: shipType,
+          purchasePrice: price,
+          timestamp: DateTime(2021),
+        );
+      }
+
+      final prices = [
+        makePrice('A-B-C', 100),
+        makePrice('A-B-D', 200),
+        makePrice('A-B-E', 300),
+        makePrice('A-B-F', 400),
+        makePrice('A-B-G', 500),
+      ];
+      final snapshot = ShipyardPriceSnapshot(prices);
+      expect(snapshot.medianPurchasePrice(shipType), 300);
+    });
   });
 }
