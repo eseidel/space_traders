a0ca04adf57b1060a96504f391fad941cafcda85
Eric Seidel <github@eseidel.com>
2025-05-09 04:10:08 +0000

    test: more ShipyardPriceSnapshot tests

--- packages/types/test/src/snapshot/shipyard_price_snapshot_test.dart
+++ packages/types/test/src/snapshot/shipyard_price_snapshot_test.dart
@@ -19,15 +19,33 @@ void main() {
         );
       }
 
-      final prices = [
+      final expectedPrices = [
         makePrice('A-B-C', 100),
         makePrice('A-B-D', 200),
         makePrice('A-B-E', 300),
         makePrice('A-B-F', 400),
         makePrice('A-B-G', 500),
       ];
-      final snapshot = ShipyardPriceSnapshot(prices);
+      final snapshot = ShipyardPriceSnapshot(expectedPrices);
       expect(snapshot.medianPurchasePrice(shipType), 300);
+
+      final prices = snapshot.purchasePricesFor(shipType);
+      expect(prices.length, 5);
+      expect(prices.map((p) => p.purchasePrice).toSet(), {
+        100,
+        200,
+        300,
+        400,
+        500,
+      });
+
+      expect(
+        snapshot.recentPurchasePrice(
+          shipyardSymbol: WaypointSymbol.fromString('A-B-C'),
+          shipType: shipType,
+        ),
+        100,
+      );
     });
   });
 }
