88ea0f553452fd6998963225295bae42ec304446
Eric Seidel <github@eseidel.com>
2025-05-23 07:10:28 -0700

    test: FleetResponse

--- packages/protocol/test/src/messages/get_fleet/get_fleet_response_test.dart
+++ packages/protocol/test/src/messages/get_fleet/get_fleet_response_test.dart
@@ -3,7 +3,32 @@ import 'package:test/test.dart';
 
 void main() {
   test(GetFleetResponse, () {
-    // Placeholder.
-    expect(true, isTrue);
+    final response = GetFleetResponse(
+      ships: [
+        FleetShip(
+          symbol: 'ship1',
+          route: ShipRoutePlan(
+            waypointSymbol: 'system1:waypoint1',
+            timeToArrival: 100,
+          ),
+          cargo: Cargo(
+            capacity: 1000,
+            units: 1000,
+            inventory: [
+              PricedItem(
+                symbol: 'item1',
+                pricePerUnit: 1000,
+                units: 1000,
+                totalPrice: 1000,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+    final json = response.toJson();
+    expect(json, isA<Map<String, dynamic>>());
+    final response2 = GetFleetResponse.fromJson(json);
+    expect(response2, equals(response));
   });
 }
