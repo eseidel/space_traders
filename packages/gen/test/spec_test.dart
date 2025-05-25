0bdac457da13a2363f5b1e3bea990993a7d3eee2
Eric Seidel <github@eseidel.com>
2025-05-24 17:12:14 -0700

    test: RefOr equality

--- packages/gen/test/spec_test.dart
+++ packages/gen/test/spec_test.dart
@@ -51,4 +51,30 @@ void main() {
       );
     });
   });
+
+  test('RefOr equality', () {
+    const bodyOne = RequestBody(
+      pointer: '#/components/requestBodies/Foo',
+      isRequired: true,
+      schema: SchemaRef.ref('#/components/schemas/Foo'),
+    );
+    const bodyTwo = RequestBody(
+      pointer: '#/components/requestBodies/Foo',
+      isRequired: true,
+      schema: SchemaRef.ref('#/components/schemas/Foo'),
+    );
+    const refOrOne = RefOr.object(bodyOne);
+    const refOrTwo = RefOr.object(bodyTwo);
+    const refOrThree = RefOr.object(
+      RequestBody(
+        pointer: '#/components/requestBodies/Bar',
+        isRequired: true,
+        schema: SchemaRef.ref('#/components/schemas/Bar'),
+      ),
+    );
+    expect(refOrOne, refOrTwo);
+    expect(refOrOne, isNot(refOrThree));
+    expect(refOrOne.hashCode, refOrTwo.hashCode);
+    expect(refOrOne.hashCode, isNot(refOrThree.hashCode));
+  });
 }
