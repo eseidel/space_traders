a53f9a656131f928c27ddcce490dcd5362e9b9b6
Eric Seidel <github@eseidel.com>
2025-05-24 22:22:16 -0700

    compiles

--- packages/cli/lib/net/counts.dart
+++ packages/cli/lib/net/counts.dart
@@ -7,32 +7,20 @@ import 'package:types/types.dart';
 /// ApiClient that counts the number of requests made.
 class CountingApiClient extends ApiClient {
   /// Construct a rate limited api client.
-  CountingApiClient({super.authentication, super.basePath});
+  CountingApiClient({super.baseUri, super.client, super.defaultHeaders});
 
   /// RequestCounts tracks the number of requests made to each path.
   final RequestCounts requestCounts = RequestCounts();
 
   @override
-  Future<Response> invokeAPI(
-    String path,
-    String method,
-    List<QueryParam> queryParams,
-    Object? body,
-    Map<String, String> headerParams,
-    Map<String, String> formParams,
-    String? contentType,
-  ) async {
+  Future<Response> invokeApi({
+    required Method method,
+    required String path,
+    Map<String, dynamic> parameters = const {},
+  }) async {
     logger.detail(path);
     requestCounts.record(path);
-    final response = await super.invokeAPI(
-      path,
-      method,
-      queryParams,
-      body,
-      headerParams,
-      formParams,
-      contentType,
-    );
+    final response = await super.invokeApi(method: method, path: path);
     // logger.info(response.body);
     return response;
   }
