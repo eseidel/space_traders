465c37971bb0cf9007392d036443ec43160ea529
Eric Seidel <github@eseidel.com>
2025-05-25 19:28:06 -0700

    feat: add two more openapi quirks

--- packages/gen/lib/src/context.dart
+++ packages/gen/lib/src/context.dart
@@ -345,7 +345,8 @@ extension _SchemaGeneration on Schema {
     var hasDefaultValue = schema.defaultValue != null;
     var defaultValueString = schema.defaultValueString;
     // OpenAPI defaults arrays to empty, so we match for now.
-    if (schema.type == SchemaType.array) {
+    if (context.quirks.allListsDefaultToEmpty &&
+        schema.type == SchemaType.array) {
       hasDefaultValue = true;
       defaultValueString = 'const []';
     }
@@ -353,7 +354,10 @@ extension _SchemaGeneration on Schema {
     // useRequired means "use the required constructor parameter"
     final useRequired = inRequiredList && !hasDefaultValue;
     // isNullable means it's optional for the server, use nullable storage.
-    final isNullable = !inRequiredList; // && !hasDefaultValue;
+    final isNullable = context.quirks.nonNullableDefaultValues
+        ? !inRequiredList && !hasDefaultValue
+        : !inRequiredList;
+
     return {
       'dartName': dartName,
       'jsonName': jsonName,
@@ -822,9 +826,21 @@ class _Context {
 
 /// Quirks are a set of flags that can be used to customize the generated code.
 class Quirks {
-  const Quirks({this.dynamicJson = false, this.mutableModels = false});
+  const Quirks({
+    this.dynamicJson = false,
+    this.mutableModels = false,
+    // Avoiding ever having List? seems reasonable so we default to true.
+    this.allListsDefaultToEmpty = true,
+    this.nonNullableDefaultValues = false,
+  });
 
-  const Quirks.openapi() : this(dynamicJson: true, mutableModels: true);
+  const Quirks.openapi()
+    : this(
+        dynamicJson: true,
+        mutableModels: true,
+        nonNullableDefaultValues: true,
+        allListsDefaultToEmpty: true,
+      );
 
   /// Use "dynamic" instead of "Map\<String, dynamic\>" for passing to fromJson
   /// to match OpenAPI's behavior.
@@ -832,6 +848,20 @@ class Quirks {
 
   /// Use mutable models instead of immutable ones to match OpenAPI's behavior.
   final bool mutableModels;
+
+  /// OpenAPI seems to have the behavior whereby all Lists default to empty
+  /// lists.
+  final bool allListsDefaultToEmpty;
+
+  /// OpenAPI seems to have the behavior whereby if a property has a default
+  /// value it can never be nullable.  Since OpenAPI also makes all Lists
+  /// default to empty lists, this means that all Lists are non-nullable.
+  final bool nonNullableDefaultValues;
+
+  // Potential future quirks:
+
+  /// OpenAPI flattens everything into the top level `lib` folder.
+  // final bool doNotUseSrcPaths;
 }
 
 void renderSpec({
