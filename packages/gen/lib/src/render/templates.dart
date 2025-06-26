f4b5999ae1bd062b264cf8d5f9d4e757d020248e
Eric Seidel <eric@shorebird.dev>
2025-06-26 09:14:49 -0800

    feat: add a template cache

--- packages/gen/lib/src/render/templates.dart
+++ packages/gen/lib/src/render/templates.dart
@@ -13,12 +13,18 @@ class TemplateProvider {
     : templateDir = const LocalFileSystem().directory('lib/templates');
 
   final Directory templateDir;
+  // Reading the same template from disk repeatedly is slow, so cache them.
+  // Saves about 4s on rendering the GitHub spec.
+  final Map<String, Template> _cache = {};
 
   Template load(String name) {
-    return Template(
-      templateDir.childFile('$name.mustache').readAsStringSync(),
-      partialResolver: load,
-      name: name,
+    return _cache.putIfAbsent(
+      name,
+      () => Template(
+        templateDir.childFile('$name.mustache').readAsStringSync(),
+        partialResolver: load,
+        name: name,
+      ),
     );
   }
 }
